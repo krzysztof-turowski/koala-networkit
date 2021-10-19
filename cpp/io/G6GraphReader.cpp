0c76a0517144bace019946399a8698ca1ed083e9
Krzysztof Turowski <krzysztof.szymon.turowski@gmail.com>
2021-10-19 23:38:14 +0200

    Fix graph6 and sparse6 code

--- cpp/io/G6GraphReader.cpp
+++ cpp/io/G6GraphReader.cpp
@@ -15,7 +15,7 @@ namespace Koala {
 
 NetworKit::Graph G6GraphReader::read(const std::string &path) {
     std::ifstream graphFile(path);
-    NetworKit::Aux::enforceOpened(graphFile);
+    Aux::enforceOpened(graphFile);
     std::string line;
     std::getline(graphFile, line);
     return readline(line);
@@ -24,49 +24,33 @@ NetworKit::Graph G6GraphReader::read(const std::string &path) {
 NetworKit::Graph G6GraphReader::readline(const std::string &line) {
     auto it = line.cbegin();
 
-    auto scanGraphOrder = [&it]() -> index {
-        const char LOW = 0x3f, HIGH = 0x7e;
-        const int SHORT_N = 2, LONG_N = 6, LENGTH = 6;
-        index nodes = 0;
-        if (*it < HIGH) {
-            nodes = *it - LOW;
-            return nodes;
+    const char LOW = 0x3f, HIGH = 0x7e, MASK = 0x20;
+    const int SHORT_N = 1, MEDIUM_N = 2, LONG_N = 6, LENGTH = 6;
+    int nodes_length = SHORT_N;
+    if (*it >= HIGH) {
+        nodes_length = MEDIUM_N, ++it;
+        if (*it >= HIGH) {
+          nodes_length = LONG_N, ++it;
         }
-        ++it;
-        if (*it < HIGH) {
-            for (int i = 0; i < SHORT_N; i++, ++it) {
-              nodes = (nodes << LENGTH) | (*it - LOW);
-            }
-            return nodes;
-        }
-        ++it;
-        for (int i = 0; i < LONG_N; i++, ++it) {
-          nodes = (nodes << LENGTH) | (*it - LOW);
-        }
-        return nodes;
-    };
+    }
 
-    auto scanNode = [&](int &v, char &mask) -> char {
-        const char LOW = 0x3f, HIGH = 0x7e, MASK = 0x20;
-        for (int u = 0; u < v; u++, mask >>= 1) {
+    NetworKit::index nodes = 0;
+    for (int i = 0; i < nodes_length; i++, ++it) {
+      nodes = (nodes << LENGTH) | (*it - LOW);
+    }
+
+    NetworKit::Graph graph(nodes, false, false);
+    char mask = 0, bits = 0;
+    for (NetworKit::index v = 1; v < nodes; v++) {
+        for (NetworKit::index u = 0; u < v; u++, mask >>= 1) {
             if (!mask) {
-                ++it;
                 assert(*it >= LOW && *it <= HIGH);
-                *it -= LOW;
-                mask = MASK;
+                bits = *it - LOW, mask = MASK, ++it;
             }
-            if (*it & mask) {
+            if (bits & mask) {
                 graph.addEdge(u, v);
             }
         }
-        return mask;
-    };
-
-    index nodes = scanGraphOrder();
-    NetworKit::Graph graph(nodes, false, false);
-    char mask = 0;
-    for (int v = 1; v < nodes; v++) {
-        mask = scanNode(v, mask);
     }
     graph.shrinkToFit();
     return graph;
--- cpp/io/S6GraphReader.cpp
+++ cpp/io/S6GraphReader.cpp
@@ -13,8 +13,8 @@
 
 namespace Koala {
 
-constexpr index log2(index n) {
-  return n > 1 ? 1 + log2(n >> 1) : 0;
+constexpr NetworKit::index log2(NetworKit::index n) {
+  return n > 2 ? 1 + log2(n >> 1) : 1;
 }
 
 NetworKit::Graph S6GraphReader::read(const std::string &path) {
@@ -30,49 +30,44 @@ NetworKit::Graph S6GraphReader::readline(const std::string &line) {
     assert(*it == ':');
     ++it;
 
-    auto scanGraphOrder = [&it]() -> index {
-        const char LOW = 0x3f, HIGH = 0x7e;
-        const int SHORT_N = 2, LONG_N = 6, LENGTH = 6;
-        index nodes = 0;
-        if (*it < HIGH) {
-            nodes = *it - LOW;
-            return nodes;
+    const char LOW = 0x3f, HIGH = 0x7e;
+    const int SHORT_N = 1, MEDIUM_N = 2, LONG_N = 6, LENGTH = 6;
+    int nodes_length = SHORT_N;
+    if (*it >= HIGH) {
+        nodes_length = MEDIUM_N, ++it;
+        if (*it >= HIGH) {
+          nodes_length = LONG_N, ++it;
         }
-        ++it;
-        if (*it < HIGH) {
-            for (int i = 0; i < SHORT_N; i++, ++it) {
-              nodes = (nodes << LENGTH) | (*it - LOW);
-            }
-            return nodes;
-        }
-        ++it;
-        for (int i = 0; i < LONG_N; i++, ++it) {
-          nodes = (nodes << LENGTH) | (*it - LOW);
-        }
-        return nodes;
-    };
+    }
 
-    auto getBitsGroup = [&]() {
-        const char LOW = 0x3f, HIGH = 0x7e;
-        const int LENGTH = 6;
-        ++it;
-        assert(*it >= LOW && *it <= HIGH);
-        bits = (bits << LENGTH) + (*it - LOW);
-        length += LENGTH;
-    };
+    NetworKit::index nodes = 0;
+    for (int i = 0; i < nodes_length; i++, ++it) {
+      nodes = (nodes << LENGTH) | (*it - LOW);
+    }
 
-    index nodes = scanGraphOrder();
-    Graph graph(nodes, false, false);
-
-    char MASKS[] = { 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40 };
-    index u = 0, v = 0, lognodes = log2(nodes), length = 0;
+    NetworKit::Graph graph(nodes, false, false);
+    const char MASKS[] = { 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40 };
+    int lognodes = log2(nodes), length = 0, bits = 0;
+    NetworKit::index u = 0, v = 0;
     while (true) {
         if (length == 0) {
-            getBitsGroup();
+            if (it == line.cend()) {
+                break;
+            }
+            assert(*it >= LOW && *it <= HIGH);
+            bits = (bits << LENGTH) | (*it - LOW);
+            length += LENGTH, ++it;
         }
-        --length, v += (bits & MASKS[length]), bits ^= MASKS[length];
+        if (bits & MASKS[--length]) {
+            v++, bits &= ~MASKS[length];
+        }
         while (length < lognodes) {
-            getBitsGroup();
+            if (it == line.cend()) {
+                break;
+            }
+            assert(*it >= LOW && *it <= HIGH);
+            bits = (bits << LENGTH) | (*it - LOW);
+            length += LENGTH, ++it;
         }
         length -= lognodes, u = bits >> length, bits ^= (u << length);
         if (u >= nodes) {
