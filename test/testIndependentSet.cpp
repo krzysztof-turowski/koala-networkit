ddfbf0b56b34d255867a98eb36cad3e66ff389f5
Onchao <artur.salawa789@gmail.com>
2023-03-21 00:47:32 +0100

    simple tests refactored

--- test/testIndependentSet.cpp
+++ test/testIndependentSet.cpp
@@ -9,225 +9,96 @@ struct IndependentSetParameters {
     int expectedSetSize;
 };
 
-class BruteForceIndependentSetTest
-    : public testing::TestWithParam<IndependentSetParameters> { };
-class Mis1IndependentSetTest
-    : public testing::TestWithParam<IndependentSetParameters> { };
-class Mis2IndependentSetTest
-    : public testing::TestWithParam<IndependentSetParameters> { };
-class Mis3IndependentSetTest
-    : public testing::TestWithParam<IndependentSetParameters> { };
-class Mis4IndependentSetTest
-    : public testing::TestWithParam<IndependentSetParameters> { };
-class Mis5IndependentSetTest
-    : public testing::TestWithParam<IndependentSetParameters> { };
-
-NetworKit::Graph build_graph(const int &N, const std::list<std::pair<int, int>> &E) {
-    NetworKit::Graph G(N, false, false);
-    for (const auto &[u, v] : E) {
-        G.addEdge(u, v);
+template <typename T> 
+class SimpleGraphs : public testing::Test {
+public:
+    virtual void SetUp() {
     }
-    return G;
-}
 
-namespace {
+protected:
+    void verify(IndependentSetParameters& parameters) {
+        NetworKit::Graph G = build_graph(parameters.N, parameters.E);
+        auto algorithm = T(G);
+        algorithm.run();
+
+        auto independentSet = algorithm.getIndependentSet();
+        for (const auto &[u, v] : parameters.E) {
+            EXPECT_FALSE(independentSet[u] && independentSet[v]);
+        }
+
+        int setSize = 0;
+        for (const auto &[v, belongs] : independentSet) {
+            setSize += (belongs);
+        }
+        EXPECT_EQ(setSize, parameters.expectedSetSize);
+    }
+
+    NetworKit::Graph build_graph(const int &N, const std::list<std::pair<int, int>> &E) {
+        NetworKit::Graph G(N, false, false);
+        for (const auto &[u, v] : E) {
+            G.addEdge(u, v);
+        }
+        return G;
+    }
+};
+
+TYPED_TEST_CASE_P(SimpleGraphs);
+
 /*
-simpleGraphs except {0, 1} are from:
+the following graphs are from:
 https://mathworld.wolfram.com/IndependentSet.html
 */
-std::vector<IndependentSetParameters> simpleGraphs {
-    {1, {
-        
-        },
-    1},
-    {2, {
-        {0, 1},
-        },
-    1},
-    {3, {
-        {0, 1}, {1, 2}
-        },
-    2},
-    {6, {
-        {0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 0}, 
-        },
-    3},
-    // {26, {
-    //     // 0 solo // = 1
 
-    //     {1, 2}, {2, 3}, // = 2
-    //     {4, 5}, {5, 6}, {6, 4}, // = 1
-
-    //     {7, 8}, {8, 9}, {9, 10}, // = 2
-    //     {11, 12}, {12, 13}, {13, 14}, {14, 11}, // = 2
-
-    //     {15, 16}, {16, 17}, {17, 18}, {18, 19}, // = 3
-    //     {20, 21}, {21, 22}, {22, 23}, {23, 24}, {24, 25}, // = 3
-
-    //     //{, }, 
-    //     },
-    // 14},
+TYPED_TEST_P(SimpleGraphs, WheelGraphW_8) {
+    IndependentSetParameters parameters = 
     {8, {
         {0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 0},
         {7, 0}, {7, 1}, {7, 2}, {7, 3}, {7, 4}, {7, 5}, {7, 6},
         },
-    3},
+    3};
+    this->verify(parameters);
+}
+
+TYPED_TEST_P(SimpleGraphs, UtilityGraphK_3_3) {
+    IndependentSetParameters parameters = 
     {6, {
         {0, 3}, {0, 4}, {0, 5}, 
         {1, 3}, {1, 4}, {1, 5},
         {2, 3}, {2, 4}, {2, 5}
         },
-    3},
+    3};
+    this->verify(parameters);
+}
+
+TYPED_TEST_P(SimpleGraphs, PetersenGraph) {
+    IndependentSetParameters parameters = 
     {10, {
         {0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 0},
         {0, 5}, {1, 6}, {2, 7}, {3, 8}, {4, 9},
         {5, 7}, {5, 8}, {6, 8}, {6, 9}, {7, 9}
         },
-    4},
+    4};
+    this->verify(parameters);
+}
+
+TYPED_TEST_P(SimpleGraphs, FruchtGraph) {
+    IndependentSetParameters parameters = 
     {12, {
         {0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 0},
         {0, 7}, {1, 8}, {2, 8}, {3, 9}, {4, 9}, {5, 10}, {6, 10},
         {7, 8}, {11, 7}, {11, 9}, {11, 10}   
     },
-    5}
-};
-} /* unnamed namespace */
-
-TEST_P(BruteForceIndependentSetTest, test) {
-    IndependentSetParameters const& parameters = GetParam();
-    NetworKit::Graph G = build_graph(parameters.N, parameters.E);
-
-    auto algorithm = Koala::BruteForceIndependentSet(G);
-    algorithm.run();
-
-    auto independentSet = algorithm.getIndependentSet();
-    for (const auto &[u, v] : parameters.E) {
-        EXPECT_FALSE(independentSet[u] && independentSet[v]);
-    }
-
-    int setSize = 0;
-    for (const auto &[v, belongs] : independentSet) {
-        setSize += (belongs);
-    }
-    EXPECT_EQ(setSize, parameters.expectedSetSize);
+    5};
+    this->verify(parameters);
 }
 
-INSTANTIATE_TEST_SUITE_P(
-    test_example, BruteForceIndependentSetTest, testing::ValuesIn(simpleGraphs)
-);
+REGISTER_TYPED_TEST_CASE_P(SimpleGraphs, WheelGraphW_8, UtilityGraphK_3_3, PetersenGraph, FruchtGraph);
 
-
-TEST_P(Mis1IndependentSetTest, test) {
-    IndependentSetParameters const& parameters = GetParam();
-    NetworKit::Graph G = build_graph(parameters.N, parameters.E);
-
-    auto algorithm = Koala::Mis1IndependentSet(G);
-    algorithm.run();
-
-    auto independentSet = algorithm.getIndependentSet();
-    for (const auto &[u, v] : parameters.E) {
-        EXPECT_FALSE(independentSet[u] && independentSet[v]);
-    }
-
-    int setSize = 0;
-    for (const auto &[v, belongs] : independentSet) {
-        setSize += (belongs);
-    }
-    EXPECT_EQ(setSize, parameters.expectedSetSize);
-}
-
-INSTANTIATE_TEST_SUITE_P(
-    test_example, Mis1IndependentSetTest, testing::ValuesIn(simpleGraphs)
-);
-
-TEST_P(Mis2IndependentSetTest, test) {
-    IndependentSetParameters const& parameters = GetParam();
-    NetworKit::Graph G = build_graph(parameters.N, parameters.E);
-
-    auto algorithm = Koala::Mis2IndependentSet(G);
-    algorithm.run();
-
-    auto independentSet = algorithm.getIndependentSet();
-    for (const auto &[u, v] : parameters.E) {
-        EXPECT_FALSE(independentSet[u] && independentSet[v]);
-    }
-
-    int setSize = 0;
-    for (const auto &[v, belongs] : independentSet) {
-        setSize += (belongs);
-    }
-    EXPECT_EQ(setSize, parameters.expectedSetSize);
-}
-
-INSTANTIATE_TEST_SUITE_P(
-    test_example, Mis2IndependentSetTest, testing::ValuesIn(simpleGraphs)
-);
-
-TEST_P(Mis3IndependentSetTest, test) {
-    IndependentSetParameters const& parameters = GetParam();
-    NetworKit::Graph G = build_graph(parameters.N, parameters.E);
-
-    auto algorithm = Koala::Mis3IndependentSet(G);
-    algorithm.run();
-
-    auto independentSet = algorithm.getIndependentSet();
-    for (const auto &[u, v] : parameters.E) {
-        EXPECT_FALSE(independentSet[u] && independentSet[v]);
-    }
-
-    int setSize = 0;
-    for (const auto &[v, belongs] : independentSet) {
-        setSize += (belongs);
-    }
-    EXPECT_EQ(setSize, parameters.expectedSetSize);
-}
-
-INSTANTIATE_TEST_SUITE_P(
-    test_example, Mis3IndependentSetTest, testing::ValuesIn(simpleGraphs)
-);
-
-TEST_P(Mis4IndependentSetTest, test) {
-    IndependentSetParameters const& parameters = GetParam();
-    NetworKit::Graph G = build_graph(parameters.N, parameters.E);
-
-    auto algorithm = Koala::Mis4IndependentSet(G);
-    algorithm.run();
-
-    auto independentSet = algorithm.getIndependentSet();
-    for (const auto &[u, v] : parameters.E) {
-        EXPECT_FALSE(independentSet[u] && independentSet[v]);
-    }
-
-    int setSize = 0;
-    for (const auto &[v, belongs] : independentSet) {
-        setSize += (belongs);
-    }
-    EXPECT_EQ(setSize, parameters.expectedSetSize);
-}
-
-INSTANTIATE_TEST_SUITE_P(
-    test_example, Mis4IndependentSetTest, testing::ValuesIn(simpleGraphs)
-);
-
-TEST_P(Mis5IndependentSetTest, test) {
-    IndependentSetParameters const& parameters = GetParam();
-    NetworKit::Graph G = build_graph(parameters.N, parameters.E);
-
-    auto algorithm = Koala::Mis5IndependentSet(G);
-    algorithm.run();
-
-    auto independentSet = algorithm.getIndependentSet();
-    for (const auto &[u, v] : parameters.E) {
-        EXPECT_FALSE(independentSet[u] && independentSet[v]);
-    }
-
-    int setSize = 0;
-    for (const auto &[v, belongs] : independentSet) {
-        setSize += (belongs);
-    }
-    EXPECT_EQ(setSize, parameters.expectedSetSize);
-}
-
-INSTANTIATE_TEST_SUITE_P(
-    test_example, Mis5IndependentSetTest, testing::ValuesIn(simpleGraphs)
-);
+typedef testing::Types<
+    Koala::BruteForceIndependentSet, 
+    Koala::Mis1IndependentSet, 
+    Koala::Mis2IndependentSet,
+    Koala::Mis3IndependentSet,
+    Koala::Mis4IndependentSet,
+    Koala::Mis5IndependentSet> Algorithms;
+INSTANTIATE_TYPED_TEST_CASE_P(IndependentSet, SimpleGraphs, Algorithms);
