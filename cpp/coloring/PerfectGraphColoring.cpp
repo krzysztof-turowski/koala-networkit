291ac6a27a3f4e54de6cab1e0fedf97881ab1df1
Krzysztof Turowski <krzysztof.szymon.turowski@gmail.com>
2023-04-03 15:21:40 +0200

    Fix

--- cpp/coloring/PerfectGraphColoring.cpp
+++ cpp/coloring/PerfectGraphColoring.cpp
@@ -10,6 +10,7 @@
 #include <tuple>
 
 #include <coloring/PerfectGraphColoring.hpp>
+#include <graph/GraphTools.hpp>
 
 #include "perfect/commons.h"
 #include "perfect/theta.h"
@@ -28,88 +29,82 @@ Graph generate_graph(std::optional<NetworKit::Graph> graph) {
 }
 
 NetworKit::Graph retrieve_graph(const Graph &G) {
-  NetworKit::Graph graph(G.n, false, false);
-  for (int i = 0; i < G.n; i++) {
-    for (int j = i + 1; j < G.n; j++) {
-      if (G.areNeighbours(i, j)) {
-        graph.addEdge(i, j);
-      }
+    NetworKit::Graph graph(G.n, false, false);
+    for (int i = 0; i < G.n; i++) {
+        for (int j = i + 1; j < G.n; j++) {
+          if (G.areNeighbours(i, j)) {
+              graph.addEdge(i, j);
+          }
+        }
     }
-  }
-  return graph;
+    return graph;
 }
 
-int getTheta(const Graph &G, const std::vector<int> &isNodeRemoved) {
-  NetworKit::Graph graph(retrieve_graph(G));
-  std::vector<int> keep_nodes(isNodeRemoved.size());
-  for (int i = 0; i < isNodeRemoved.size(); i++) {
-    keep_nodes[i] = !isNodeRemoved[i];
-  }
-  std::vector<int> indices(graph.numberOfNodes());
-  std::partial_sum(keep_nodes.begin(), keep_nodes.end(), indices.begin());
-  if (indices.empty() || indices.back() == 0) {
-    return 0;
-  }
+int getTheta(const NetworKit::Graph &graph, const std::vector<int> &keep_nodes) {
+    std::vector<int> indices(graph.numberOfNodes());
+    std::partial_sum(keep_nodes.begin(), keep_nodes.end(), indices.begin());
+    if (indices.empty() || indices.back() == 0) {
+        return 0;
+    }
 
-  int m = 0;
-  std::vector<int> from, to;
-  graph.forEdges([&](NetworKit::node u, NetworKit::node v) {
-      if (keep_nodes[u] && keep_nodes[v]) {
-          from.push_back(indices[u]), to.push_back(indices[v]), m++;
-          assert(G.areNeighbours(u, v));
-      }
-  });
+    int m = 0;
+    std::vector<int> from, to;
+    graph.forEdges([&](NetworKit::node u, NetworKit::node v) {
+        if (keep_nodes[u] && keep_nodes[v]) {
+            from.push_back(indices[u]), to.push_back(indices[v]), m++;
+        }
+    });
 
-  if (indices.back() == 0) {
-      return 0;
-  }
-  if (indices.back() == 1) {
-      return 1;
-  }
-  if (indices.back() == 2) {
-      return 1 + (m == 0);
-  }
+    if (indices.back() == 0) {
+        return 0;
+    }
+    if (indices.back() == 1) {
+        return 1;
+    }
+    if (indices.back() == 2) {
+        return 1 + (m == 0);
+    }
 
-  auto e = std::make_tuple(indices.back(), m, from, to);
-  static std::map<std::tuple<int, int, std::vector<int>, std::vector<int>>, int> CACHE;
-  if (CACHE.count(e) > 0) {
-    return CACHE[e];
-  }
+    auto e = std::make_tuple(indices.back(), m, from, to);
+    static std::map<std::tuple<int, int, std::vector<int>, std::vector<int>>, int> CACHE;
+    if (CACHE.count(e) > 0) {
+        return CACHE[e];
+    }
 
-  double theta = get_theta(indices.back(), m, from.data(), to.data());
-  int theta_int = theta + 0.5;
-  double eps = 0.3;
-  if (abs(theta - theta_int) > eps) {
-    throw std::logic_error("Theta returned non-integer for a Perfect Graph: " + std::to_string(theta));
-  }
-  return CACHE[e] = theta_int;
+    double theta = get_theta(indices.back(), m, from.data(), to.data());
+    int theta_int = theta + 0.5;
+    double eps = 0.3;
+    if (abs(theta - theta_int) > eps) {
+        throw std::logic_error("Theta returned non-integer for a Perfect Graph: " + std::to_string(theta));
+    }
+    return CACHE[e] = theta_int;
 }
 
 int getOmega(const Graph &G) {
-  return getTheta(G.getComplement(), std::vector<int>(G.n, 0));
+  NetworKit::Graph graph(retrieve_graph(G));
+  auto graph_complement = Koala::GraphTools::toComplement(graph);
+  return getTheta(graph_complement, std::vector<int>(G.n, 1));
 }
 
-std::vector<int> getMaxCardStableSet(const Graph &G) {
-  int thetaG = getTheta(G, std::vector<int>(G.n, 0));
-
-  std::vector<int> isNodeRemoved(G.n, 0);
+std::vector<int> getMaxCardStableSet(const NetworKit::Graph &graph) {
+  int thetaG = getTheta(graph, std::vector<int>(graph.numberOfNodes(), 1));
+  std::vector<int> isNodeRemoved(graph.numberOfNodes(), 1);
   std::vector<int> res;
-
-  for (int i = 0; i < G.n; i++) {
-    isNodeRemoved[i] = 1;
-    int newTheta = getTheta(G, isNodeRemoved);
-
-    if (newTheta != thetaG) {
-      isNodeRemoved[i] = 0;
-      res.push_back(i);
-    }
-  }
-
+  graph.forNodes([&](NetworKit::node v) {
+      isNodeRemoved[v] = 0;
+      int newTheta = getTheta(graph, isNodeRemoved);
+      if (newTheta != thetaG) {
+        isNodeRemoved[v] = 1;
+        res.push_back(v);
+      }
+  });
   return res;
 }
 
 std::vector<int> getMaxCardClique(const Graph &G) {
-  return getMaxCardStableSet(G.getComplement());
+  NetworKit::Graph graph(retrieve_graph(G));
+  auto graph_complement = Koala::GraphTools::toComplement(graph);
+  return getMaxCardStableSet(graph_complement);
 }
 
 std::vector<int> getSSIntersectingCliques(const Graph &G, std::vector<std::vector<int>> K) {
@@ -122,26 +117,23 @@ std::vector<int> getSSIntersectingCliques(const Graph &G, std::vector<std::vecto
   }
   std::partial_sum(c.begin(), c.end(), c.begin());
   std::vector<std::vector<int>> nneighbors(c.back());
+  NetworKit::Graph subgraph(c.back(), false, false);
   for (int i = 0; i < G.n; i++) {
-    for (int j = i + 1; j < G.n; j++) {
-      if (G.areNeighbours(i, j)) {
-        for (int ni = i == 0 ? 0 : c[i - 1]; ni < c[i]; ni++) {
-          for (int nj = c[j - 1]; nj < c[j]; nj++) {
-            nneighbors[ni].push_back(nj);
-            nneighbors[nj].push_back(ni);
-          }
+      for (int j = i + 1; j < G.n; j++) {
+          if (G.areNeighbours(i, j)) {
+            for (int ni = i == 0 ? 0 : c[i - 1]; ni < c[i]; ni++) {
+                for (int nj = c[j - 1]; nj < c[j]; nj++) {
+                    subgraph.addEdge(ni, nj);
+                }
+            }
         }
       }
-    }
   }
-
-  Graph nG(nneighbors);
-  std::vector<int> nSS = getMaxCardStableSet(Graph(nneighbors));
+  std::vector<int> nSS = getMaxCardStableSet(subgraph);
   std::vector<int> ret;
   int wsk = 0;
   for (int nSSnode : nSS) {
     while (wsk < G.n && c[wsk] <= nSSnode) wsk++;
-
     if (ret.empty() || ret.back() != wsk) {
       ret.push_back(wsk);
     }
