fa9f55b9de63e97666843090d016b9da23a29600
Krzysztof Turowski <krzysztof.szymon.turowski@gmail.com>
2023-04-04 07:59:05 +0200

    Remove theta.cpp

--- cpp/coloring/PerfectGraphColoring.cpp
+++ cpp/coloring/PerfectGraphColoring.cpp
@@ -10,41 +10,71 @@
 #include <optional>
 #include <tuple>
 
+extern "C" {
+#include <declarations.h>
+}
+
 #include <coloring/PerfectGraphColoring.hpp>
 #include <graph/GraphTools.hpp>
 
 #include "perfect/commons.h"
-#include "perfect/theta.h"
 
-Graph generate_graph(std::optional<NetworKit::Graph> graph) {
-    std::map<NetworKit::node, int> vertices;
-    int nodes = 0;
-    graph->forNodes([&](NetworKit::node v) {
-        vertices[v] = nodes++;
-    });
-    std::vector<std::vector<int>> M(nodes);
-    graph->forEdges([&](NetworKit::node u, NetworKit::node v) {
-        M[vertices[u]].push_back(vertices[v]), M[vertices[v]].push_back(vertices[u]);
-    });
-    return Graph(M);
-}
+// Input: Graph of n vertices and m edges. Arrays from and to of size m, from[i]->to[i] is i'th edge.
+// Nodes start at 1.
+double get_theta(int n, int m, int *from, int *to) {
+    double pobj, dobj, *y, *a;
+    struct blockmatrix C, X, Z;
+    struct constraintmatrix *constraints;
 
-NetworKit::Graph retrieve_graph(const Graph &G) {
-    NetworKit::Graph graph(G.n, false, false);
-    for (int i = 0; i < G.n; i++) {
-        for (int j = i + 1; j < G.n; j++) {
-          if (G.areNeighbours(i, j)) {
-              graph.addEdge(i, j);
-          }
+    C.nblocks = 1;
+    C.blocks = static_cast<blockrec*>(malloc(2 * sizeof(struct blockrec)));
+    C.blocks[1].blockcategory = MATRIX;
+    C.blocks[1].blocksize = n;
+    C.blocks[1].data.mat = static_cast<double*>(malloc(n * n * sizeof(double)));
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            C.blocks[1].data.mat[ijtok(i, j, n)] = 1.0;
         }
     }
-    return graph;
+    a = static_cast<double*>(malloc((m + 2)* sizeof(double)));
+    a[1] = 1.0;
+    constraints = static_cast<struct constraintmatrix*>(malloc((m + 2) * sizeof(struct constraintmatrix)));
+    constraints[1].blocks = static_cast<struct sparseblock*>(malloc(sizeof(struct sparseblock)));
+    constraints[1].blocks->blocknum = constraints[1].blocks->constraintnum = 1;
+    constraints[1].blocks->numentries = constraints[1].blocks->blocksize = n;
+    constraints[1].blocks->next = constraints[1].blocks->nextbyblock = NULL;
+    constraints[1].blocks->entries = static_cast<double*>(malloc((n + 1) * sizeof(double)));
+    constraints[1].blocks->iindices = static_cast<int*>(malloc((n + 1) * sizeof(int)));
+    constraints[1].blocks->jindices = static_cast<int*>(malloc((n + 1) * sizeof(int)));
+    for (int i = 1; i <= n; i++) {
+        constraints[1].blocks->entries[i] = 1.0;
+        constraints[1].blocks->iindices[i] = constraints[1].blocks->jindices[i] = i;
+    }
+    for (int i = 2; i <= m + 1; i++) {
+        a[i] = 0.0;
+        constraints[i].blocks = static_cast<struct sparseblock*>(malloc(sizeof(struct sparseblock)));
+        constraints[i].blocks->blocknum = constraints[i].blocks->numentries = 1;
+        constraints[i].blocks->blocksize = n;
+        constraints[i].blocks->constraintnum = i;
+        constraints[i].blocks->next = constraints[i].blocks->nextbyblock = NULL;
+        constraints[i].blocks->entries = static_cast<double*>(malloc(2 * sizeof(double)));
+        constraints[i].blocks->iindices = static_cast<int*>(malloc(2 * sizeof(int)));
+        constraints[i].blocks->jindices = static_cast<int*>(malloc(2 * sizeof(int)));
+        constraints[i].blocks->entries[1] = 1.0;
+        int start = from[i - 2], finish = to[i - 2];
+        if (start > finish) {
+            std::swap(start, finish);
+        }
+        constraints[i].blocks->iindices[1] = start;
+        constraints[i].blocks->jindices[1] = finish;
+    }
+    initsoln(n, m + 1, C, a, constraints, &X, &y, &Z);
+    easy_sdp(n, m + 1, C, a, constraints, 0.0, &X, &y, &Z, &pobj, &dobj);
+    free_prob(n, m + 1, C, a, constraints, X, y, Z);
+    return (dobj + pobj) / 2;
 }
 
 int getTheta(const NetworKit::Graph &graph, const std::vector<int> &keep_nodes) {
-        printf("KEEP NODES: ");
-        for(auto v : keep_nodes) printf("%d ", v);
-        printf("\n");
     std::vector<int> indices(keep_nodes.size());
     std::partial_sum(keep_nodes.begin(), keep_nodes.end(), indices.begin());
     if (indices.empty() || indices.back() == 0) {
@@ -58,10 +88,8 @@ int getTheta(const NetworKit::Graph &graph, const std::vector<int> &keep_nodes) 
     graph.forEdges([&](NetworKit::node u, NetworKit::node v) {
         if (keep_nodes[u] && keep_nodes[v]) {
             from.push_back(indices[u]), to.push_back(indices[v]);
-            printf("(%d,%d) ", from.back(), to.back());
         }
     });
-    printf("\n");
     if (indices.back() == 2) {
         return 1 + (from.size() == 0);
     }
@@ -93,12 +121,6 @@ int getOmega(const NetworKit::Graph &graph) {
     return getTheta(Koala::GraphTools::toComplement(graph), get_vector(graph));
 }
 
-int getOmega(const Graph &G) {
-    NetworKit::Graph graph(retrieve_graph(G));
-    auto graph_complement = Koala::GraphTools::toComplement(graph);
-    return getTheta(graph_complement, get_vector(graph_complement));
-}
-
 std::vector<int> get_maximum_stable_set(const NetworKit::Graph &graph) {
     std::vector<int> keep_nodes(get_vector(graph));
     int theta = getTheta(graph, keep_nodes);
@@ -109,12 +131,6 @@ std::vector<int> get_maximum_stable_set(const NetworKit::Graph &graph) {
         }
     });
     return keep_nodes;
-}
-
-std::vector<int> get_maximum_clique(const Graph &G) {
-  NetworKit::Graph graph(retrieve_graph(G));
-  auto graph_complement = Koala::GraphTools::toComplement(graph);
-  return get_maximum_stable_set(graph_complement);
 }
 
 std::vector<int> get_maximum_clique(const NetworKit::Graph &graph) {
@@ -137,140 +153,27 @@ int PerfectGraphColoring::get_chi() {
 }
 
 std::vector<NetworKit::node> PerfectGraphColoring::get_stable_set_intersecting_all_maximum_cliques() {
-    Graph G(generate_graph(graph));
-    std::vector<int> vertices;
-    std::map<int, int> inv_vertices;
-    int nodes = 0;
-    graph->forNodes([&](NetworKit::node v) {
-        vertices.push_back(v);
-        inv_vertices[v] = nodes++;
-    });
-    auto K = get_maximum_clique(G);
     auto K2 = get_maximum_clique(*graph);
-    int omega = std::accumulate(K.begin(), K.end(), 0);
     int omega2 = std::accumulate(K2.begin(), K2.end(), 0);
-    assert(omega == omega2);
-        printf("map: ");
-        for (int i = 0; i < vertices.size(); i++)
-          printf("%d:%d ", i, vertices[i]);
-        printf("\n");
-        printf("G1: ");
-        for (int i = 0; i < G.n; i++) for (int j = i + 1; j < G.n; j++)
-          if (G.areNeighbours(i, j)) printf("(%d,%d) ", i, j);
-        printf("\n");
-        printf("G1: ");
-        graph->forEdges([&](NetworKit::node u, NetworKit::node v) {
-            printf("(%d,%d) ", u, v);
-        });
-        printf("\n");
-        printf("N1: ");
-        graph->forNodes([&](NetworKit::node v) {
-            printf("%d ", v);
-        });
-        printf("\n");
-        for (int i = 0; i < G.n; i++) for (int j = i + 1; j < G.n; j++)
-          if (G.areNeighbours(i, j)) assert(graph->hasEdge(vertices[i], vertices[j]));
     while (true) {
-        std::vector<int> S = get_stable_set_intersecting_maximum_cliques(K);
-        std::vector<int> compS = getComplementNodesVec(G.n, S);
         std::vector<int> S2 = get_stable_set_intersecting_maximum_cliques_2(K2);
-        int s1 = S.size(), s2 = S2.size();
-        printf("s: %d %d\n", s1, s2);
-        printf("S1: ");
-        for(auto v : S) printf("%d (%d) ", v, vertices[v]);
-        printf("\n");
-        printf("S2: ");
-        for(auto v : S2) printf("%d ", v);
-        printf("\n");
-        assert(s1 == s2);
-        printf("G: ");
-        graph->forNodes([&](NetworKit::node v) {
-            printf("%d ", v);
-        });
-        printf("\n");
-        for (int i = 0; i < S.size(); i++) {
-            assert(graph->hasNode(S2[i]));
-            assert(vertices[S[i]] == S2[i]);
-        }
-        Graph Gprim = G.getInducedStrong(compS);
         NetworKit::Graph subgraph2 = *graph;
         for (auto v : S2) {
             subgraph2.removeNode(v);
         }
-        assert(Gprim.n == subgraph2.numberOfNodes());
-        int omega3 = getOmega(Gprim);
         int omega4 = getOmega(subgraph2);
-        printf("omega: %d %d\n", omega3, omega4);
-        assert(getOmega(Gprim) == getOmega(subgraph2));
-        if (getOmega(Gprim) < omega) {
+        if (omega4 < omega2) {
             std::vector<NetworKit::node> out;
-            for (auto i : S) {
-                out.push_back(vertices[i]);
+            for (auto i : S2) {
+                out.push_back(i);
             }
             return out;
         }
-        printf("map: ");
-        for (int i = 0; i < compS.size(); i++)
-          printf("%d:%d (%d) ", i, compS[i], vertices[compS[i]]);
-        printf("\n");
-        printf("G2: ");
-        for (int i = 0; i < Gprim.n; i++) for (int j = i + 1; j < Gprim.n; j++)
-          if (Gprim.areNeighbours(i, j)) printf("(%d,%d) ", i, j);
-        printf("\n");
-        printf("G2: ");
-        subgraph2.forEdges([&](NetworKit::node u, NetworKit::node v) {
-            printf("(%d,%d) ", u, v);
-        });
-        printf("\n");
-        for (int i = 0; i < Gprim.n; i++) for (int j = i + 1; j < Gprim.n; j++)
-          if (Gprim.areNeighbours(i, j)) assert(subgraph2.hasEdge(vertices[compS[i]], vertices[compS[j]]));
-        auto subgraph = retrieve_graph(Gprim);
-        auto clique = get_maximum_clique(Gprim);
-        auto clique2 = get_maximum_clique(subgraph);
         auto clique3 = get_maximum_clique(subgraph2);
-        assert(std::accumulate(clique.begin(), clique.end(), 0) == std::accumulate(clique2.begin(), clique2.end(), 0));
-        assert(std::accumulate(clique.begin(), clique.end(), 0) == std::accumulate(clique3.begin(), clique3.end(), 0));
-        for (int i = 0; i < clique.size(); i++) {
-            K[compS[i]] += clique[i];
-        }
         for (int i = 0; i < clique3.size(); i++) {
             K2[i] += clique3[i];
         }
     }
-}
-
-std::vector<int> PerfectGraphColoring::get_stable_set_intersecting_maximum_cliques(const std::vector<int> &K) {
-    printf("K:  ");
-    for(auto v : K) printf("%d ", v);
-    printf("\n");
-    std::vector<int> c(K.size());
-    std::map<NetworKit::node, int> V;
-    int nodes = 0;
-    graph->forNodes([&](NetworKit::node v) {
-        V[v] = nodes++;
-    });
-    std::partial_sum(K.begin(), K.end(), c.begin());
-    NetworKit::Graph auxiliary_graph(c.back(), false, false);
-    graph->forEdges([&](NetworKit::node u, NetworKit::node v) {
-        for (int ni = V[u] == 0 ? 0 : c[V[u] - 1]; ni < c[V[u]]; ni++) {
-            for (int nj = V[v] == 0 ? 0 : c[V[v] - 1]; nj < c[V[v]]; nj++) {
-                auxiliary_graph.addEdge(ni, nj);
-            }
-        }
-    });
-    auto stable_set = get_maximum_stable_set(auxiliary_graph);
-    std::vector<int> out;
-    for (int i = 0, index = 0; i < stable_set.size(); i++) {
-        if (stable_set[i]) {
-            while (index < c.size() && c[index] <= i) {
-                index++;
-            }
-            if (out.empty() || out.back() != index) {
-                out.push_back(index);
-            }
-        }
-    }
-    return out;
 }
 
 std::vector<int> PerfectGraphColoring::get_stable_set_intersecting_maximum_cliques_2(const std::vector<int> &K) {
@@ -288,9 +191,9 @@ std::vector<int> PerfectGraphColoring::get_stable_set_intersecting_maximum_cliqu
         }
     });
     auto stable_set = get_maximum_stable_set(auxiliary_graph);
-    printf("stable_set: ");
-    for(auto v : stable_set) printf("%d ", v);
-    printf("\n");
+    // printf("stable_set: ");
+    // for(auto v : stable_set) printf("%d ", v);
+    // printf("\n");
     std::vector<int> out;
     for (int i = 0, index = 0; i < stable_set.size(); i++) {
         if (stable_set[i]) {
@@ -302,9 +205,9 @@ std::vector<int> PerfectGraphColoring::get_stable_set_intersecting_maximum_cliqu
             }
         }
     }
-    printf("stable_set_out: ");
-    for(auto v : out) printf("%d ", v);
-    printf("\n");
+    // printf("stable_set_out: ");
+    // for(auto v : out) printf("%d ", v);
+    // printf("\n");
     return out;
 }
 
--- cpp/coloring/perfect/theta.h
+++
@@ -1,3 +0,0 @@
-// Input: Graph of n vertices and m edges. Arrays from and to of size m, from[i]->to[i] is i'th edge.
-// Nodes start at 1.
-double get_theta(int n, int m, int *from, int *to);
