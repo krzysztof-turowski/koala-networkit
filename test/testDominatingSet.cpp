e7838b4547437f6fcbe26ad10d1814012737574e
Piotr Kubaty <piotr.kubaty@student.uj.edu.pl>
2023-07-04 23:38:30 +0200

    rewrite tests to match other tests convention

--- test/testDominatingSet.cpp
+++ test/testDominatingSet.cpp
@@ -1,8 +1,4 @@
-#include <algorithm>
-#include <chrono>
-#include <fstream>
-#include <iterator>
-#include <io/G6GraphReader.hpp>
+#include <gtest/gtest.h>
 
 #include <dominating_set/BranchAndReduceMDS.hpp>
 #include <dominating_set/BranchAndReduceMSC.hpp>
@@ -10,118 +6,222 @@
 #include <dominating_set/FominKratschWoeginger.hpp>
 #include <dominating_set/SchiermeyerMDS.hpp>
 
-template <typename A>
-void test_algo_g6(const std::string inputFileName) {
-    std::ifstream inputStream(inputFileName);
-    assert(inputStream.good());  
-    std::string g6Graph;
-    int processed = 0;
-    while (std::getline(inputStream, g6Graph)) {
-        Koala::G6GraphReader reader;
-        NetworKit::Graph graph = reader.readline(g6Graph);
-        A algo(graph);
-        ExhaustiveMDS exhaustive(graph);
+#include "helpers.hpp"
 
-        algo.run();
-        exhaustive.run();
+struct MinimumDominatingSetParameters {
+    int N;
+    std::list<std::pair<int, int>> E;
+    int minimumDominatingSetSize;
+};
 
-        assert(algo.isDominating(algo.getDominatingSet()));
-        assert(exhaustive.isDominating(exhaustive.getDominatingSet()));
-        assert(
-            MinimumDominatingSet::dominatingSetSize(algo.getDominatingSet()) ==
-            MinimumDominatingSet::dominatingSetSize(exhaustive.getDominatingSet()));
+class GrandoniTest
+    : public testing::TestWithParam<MinimumDominatingSetParameters> {};
 
-        processed++;
-        if ((processed % 100'000) == 0) {
-            std::cout << processed << " graphs processed\n";
-        }
+class FominGrandoniKratschTest
+    : public testing::TestWithParam<MinimumDominatingSetParameters> {};
+
+class RooijBodlaenderTest
+    : public testing::TestWithParam<MinimumDominatingSetParameters> {};
+
+class FominKratschWoegingerTest
+    : public testing::TestWithParam<MinimumDominatingSetParameters> {};
+
+class SchiermeyerTest
+    : public testing::TestWithParam<MinimumDominatingSetParameters> {};
+
+class ExhaustiveTest
+    : public testing::TestWithParam<MinimumDominatingSetParameters> {};
+
+NetworKit::Graph build_graph(const int &N, const std::list<std::pair<int, int>> &E) {
+    NetworKit::Graph G(N, false, false);
+    for (const auto &[u, v] : E) {
+        G.addEdge(u, v);
     }
+    return G;
 }
 
-template <typename A, typename B>
-void testPowerOfTwoAdjacency(bool exactlySame) {
-    int dim = 6;
-    int nNodes = 1 << dim;
-    NetworKit::Graph graph(nNodes);
-    for (int i = 0; i < dim; i++) {
-        int diff = 1 << i;
-        for (int v = 0; v < nNodes - diff; v++) {
-            graph.addEdge(v, v + diff);
-        }
-    }
-
-    A algorithmA(graph);
-    B algorithmB(graph);
-
-    {
-        auto start = std::chrono::high_resolution_clock::now();
-        algorithmA.run();
-        auto end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-        std::cout << duration << "\n";
-    }
-    
-    {
-        auto start = std::chrono::high_resolution_clock::now();
-        algorithmB.run();
-        auto end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-        std::cout << duration << "\n";
-    }
-    assert(algorithmA.isDominating(algorithmA.getDominatingSet()));
-    assert(algorithmB.isDominating(algorithmB.getDominatingSet()));
-    assert(dsSize(algorithmA.getDominatingSet()) == dsSize(algorithmB.getDominatingSet()));
-    if (exactlySame) {
-        for (int i = 0; i < graph.numberOfNodes(); i++) {
-            assert(algorithmA.getDominatingSet().at(i) == algorithmB.getDominatingSet().at(i));
-        }
-    }
+TEST_P(GrandoniTest, test) {
+    MinimumDominatingSetParameters const& parameters = GetParam();
+    NetworKit::Graph G = build_graph(parameters.N, parameters.E);
+    auto algorithm = BranchAndReduceMDS<GrandoniMSC>(G);
+    algorithm.run();
+    EXPECT_TRUE(algorithm.isDominating(algorithm.getDominatingSet()));
+    EXPECT_EQ(
+        MinimumDominatingSet::dominatingSetSize(algorithm.getDominatingSet()),
+        parameters.minimumDominatingSetSize);
 }
 
-// https://users.cecs.anu.edu.au/~bdm/data/graphs.html
-int main() {
-    auto fileNames = {"input/graph2.g6", "input/graph3.g6", "input/graph4.g6", "input/graph5.g6", "input/graph6.g6", "input/graph7.g6", "input/graph8.g6", "input/graph9.g6"}; //"input/graph10.g6"};
-    for (auto& name : fileNames) {
-        std::cout << "run " << name << "\n";
-        test_algo_g6<FominKratschWoegingerMDS>(name);
-        test_algo_g6<SchiermeyerMDS>(name);
-        test_algo_g6<BranchAndReduceMDS<RooijBodlaenderMSC>>(name);
-        test_algo_g6<BranchAndReduceMDS<FominGrandoniKratschMSC>>(name);
-        test_algo_g6<BranchAndReduceMDS<GrandoniMSC>>(name);
-    }
+INSTANTIATE_TEST_SUITE_P(
+    test_example, GrandoniTest, testing::Values(
+        MinimumDominatingSetParameters{
+            32, {{0,1}, {0,2}, {0,4}, {0,8}, {0,16}, {1,2}, {1,3}, {1,5}, {1,9}, {1,17}, {2,3},
+                {2,4}, {2,6}, {2,10}, {2,18}, {3,4}, {3,5}, {3,7}, {3,11}, {3,19}, {4,5}, {4,6},
+                {4,8}, {4,12}, {4,20}, {5,6}, {5,7}, {5,9}, {5,13}, {5,21}, {6,7}, {6,8}, {6,10},
+                {6,14}, {6,22}, {7,8}, {7,9}, {7,11}, {7,15}, {7,23}, {8,9}, {8,10}, {8,12}, {8,16},
+                {8,24}, {9,10}, {9,11}, {9,13}, {9,17}, {9,25}, {10,11}, {10,12}, {10,14}, {10,18},
+                {10,26}, {11,12}, {11,13}, {11,15}, {11,19}, {11,27}, {12,13}, {12,14}, {12,16},
+                {12,20}, {12,28}, {13,14}, {13,15}, {13,17}, {13,21}, {13,29}, {14,15}, {14,16},
+                {14,18}, {14,22}, {14,30}, {15,16}, {15,17}, {15,19}, {15,23}, {15,31}, {16,17},
+                {16,18}, {16,20}, {16,24}, {17,18}, {17,19}, {17,21}, {17,25}, {18,19}, {18,20},
+                {18,22}, {18,26}, {19,20}, {19,21}, {19,23}, {19,27}, {20,21}, {20,22}, {20,24},
+                {20,28}, {21,22}, {21,23}, {21,25}, {21,29}, {22,23}, {22,24}, {22,26}, {22,30},
+                {23,24}, {23,25}, {23,27}, {23,31}, {24,25}, {24,26}, {24,28}, {25,26}, {25,27},
+                {25,29}, {26,27}, {26,28}, {26,30}, {27,28}, {27,29}, {27,31}, {28,29}, {28,30},
+                {29,30}, {29,31}, {30,31}}, 5}
+    )
+);
+
+TEST_P(FominGrandoniKratschTest, test) {
+    MinimumDominatingSetParameters const& parameters = GetParam();
+    NetworKit::Graph G = build_graph(parameters.N, parameters.E);
+    auto algorithm = BranchAndReduceMDS<FominGrandoniKratschMSC>(G);
+    algorithm.run();
+    EXPECT_TRUE(algorithm.isDominating(algorithm.getDominatingSet()));
+    EXPECT_EQ(
+        MinimumDominatingSet::dominatingSetSize(algorithm.getDominatingSet()),
+        parameters.minimumDominatingSetSize);
 }
 
+INSTANTIATE_TEST_SUITE_P(
+    test_example, FominGrandoniKratschTest, testing::Values(
+        MinimumDominatingSetParameters{
+            32, {{0,1}, {0,2}, {0,4}, {0,8}, {0,16}, {1,2}, {1,3}, {1,5}, {1,9}, {1,17}, {2,3},
+                {2,4}, {2,6}, {2,10}, {2,18}, {3,4}, {3,5}, {3,7}, {3,11}, {3,19}, {4,5}, {4,6},
+                {4,8}, {4,12}, {4,20}, {5,6}, {5,7}, {5,9}, {5,13}, {5,21}, {6,7}, {6,8}, {6,10},
+                {6,14}, {6,22}, {7,8}, {7,9}, {7,11}, {7,15}, {7,23}, {8,9}, {8,10}, {8,12}, {8,16},
+                {8,24}, {9,10}, {9,11}, {9,13}, {9,17}, {9,25}, {10,11}, {10,12}, {10,14}, {10,18},
+                {10,26}, {11,12}, {11,13}, {11,15}, {11,19}, {11,27}, {12,13}, {12,14}, {12,16},
+                {12,20}, {12,28}, {13,14}, {13,15}, {13,17}, {13,21}, {13,29}, {14,15}, {14,16},
+                {14,18}, {14,22}, {14,30}, {15,16}, {15,17}, {15,19}, {15,23}, {15,31}, {16,17},
+                {16,18}, {16,20}, {16,24}, {17,18}, {17,19}, {17,21}, {17,25}, {18,19}, {18,20},
+                {18,22}, {18,26}, {19,20}, {19,21}, {19,23}, {19,27}, {20,21}, {20,22}, {20,24},
+                {20,28}, {21,22}, {21,23}, {21,25}, {21,29}, {22,23}, {22,24}, {22,26}, {22,30},
+                {23,24}, {23,25}, {23,27}, {23,31}, {24,25}, {24,26}, {24,28}, {25,26}, {25,27},
+                {25,29}, {26,27}, {26,28}, {26,30}, {27,28}, {27,29}, {27,31}, {28,29}, {28,30},
+                {29,30}, {29,31}, {30,31}}, 5}
+    )
+);
 
-// void testSpecific() {
-//     int nNodes = 10;
-//     NetworKit::Graph graph(nNodes);
-//     graph.addEdge(5, 0);
-//     graph.addEdge(6, 1);
-//     graph.addEdge(6, 2);
-//     graph.addEdge(7, 1);
-//     graph.addEdge(7, 3);
-//     graph.addEdge(8, 0);
-//     graph.addEdge(8, 2);
-//     graph.addEdge(8, 4);
-//     graph.addEdge(9, 3);
-//     graph.addEdge(9, 4);
-//     graph.addEdge(9, 5);
-//     graph.addEdge(9, 8);
-//     RooijBodlaenderMDS algorithm(graph);
-//     // ExhaustiveMDS algorithm(graph);
-//     auto start = std::chrono::high_resolution_clock::now();
-//     algorithm.run();
-//     auto end = std::chrono::high_resolution_clock::now();
-//     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-//     std::cout << duration << "\n";
-//     std::vector<bool> dominating_set = algorithm.getDominatingSet();
-//     int ds_size = 0;
-//     for (bool is_in_dominating_set : dominating_set) {
-//         if (is_in_dominating_set) {
-//             ds_size++;
-//         }
-//         std::cout << is_in_dominating_set;
-//     } std::cout << " ";
-//     std::cout << "with dominating set size = " << ds_size << "\n";
-//     std::cout << (algorithm.isDominating(dominating_set) ? "correct\n" : "incorrect\n");
-// }
+TEST_P(RooijBodlaenderTest, test) {
+    MinimumDominatingSetParameters const& parameters = GetParam();
+    NetworKit::Graph G = build_graph(parameters.N, parameters.E);
+    auto algorithm = BranchAndReduceMDS<RooijBodlaenderMSC>(G);
+    algorithm.run();
+    EXPECT_TRUE(algorithm.isDominating(algorithm.getDominatingSet()));
+    EXPECT_EQ(
+        MinimumDominatingSet::dominatingSetSize(algorithm.getDominatingSet()),
+        parameters.minimumDominatingSetSize);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    test_example, RooijBodlaenderTest, testing::Values(
+        MinimumDominatingSetParameters{
+            32, {{0,1}, {0,2}, {0,4}, {0,8}, {0,16}, {1,2}, {1,3}, {1,5}, {1,9}, {1,17}, {2,3},
+                {2,4}, {2,6}, {2,10}, {2,18}, {3,4}, {3,5}, {3,7}, {3,11}, {3,19}, {4,5}, {4,6},
+                {4,8}, {4,12}, {4,20}, {5,6}, {5,7}, {5,9}, {5,13}, {5,21}, {6,7}, {6,8}, {6,10},
+                {6,14}, {6,22}, {7,8}, {7,9}, {7,11}, {7,15}, {7,23}, {8,9}, {8,10}, {8,12}, {8,16},
+                {8,24}, {9,10}, {9,11}, {9,13}, {9,17}, {9,25}, {10,11}, {10,12}, {10,14}, {10,18},
+                {10,26}, {11,12}, {11,13}, {11,15}, {11,19}, {11,27}, {12,13}, {12,14}, {12,16},
+                {12,20}, {12,28}, {13,14}, {13,15}, {13,17}, {13,21}, {13,29}, {14,15}, {14,16},
+                {14,18}, {14,22}, {14,30}, {15,16}, {15,17}, {15,19}, {15,23}, {15,31}, {16,17},
+                {16,18}, {16,20}, {16,24}, {17,18}, {17,19}, {17,21}, {17,25}, {18,19}, {18,20},
+                {18,22}, {18,26}, {19,20}, {19,21}, {19,23}, {19,27}, {20,21}, {20,22}, {20,24},
+                {20,28}, {21,22}, {21,23}, {21,25}, {21,29}, {22,23}, {22,24}, {22,26}, {22,30},
+                {23,24}, {23,25}, {23,27}, {23,31}, {24,25}, {24,26}, {24,28}, {25,26}, {25,27},
+                {25,29}, {26,27}, {26,28}, {26,30}, {27,28}, {27,29}, {27,31}, {28,29}, {28,30},
+                {29,30}, {29,31}, {30,31}}, 5}
+    )
+);
+
+TEST_P(FominKratschWoegingerTest, test) {
+    MinimumDominatingSetParameters const& parameters = GetParam();
+    NetworKit::Graph G = build_graph(parameters.N, parameters.E);
+    auto algorithm = FominKratschWoegingerMDS(G);
+    algorithm.run();
+    EXPECT_TRUE(algorithm.isDominating(algorithm.getDominatingSet()));
+    EXPECT_EQ(
+        MinimumDominatingSet::dominatingSetSize(algorithm.getDominatingSet()),
+        parameters.minimumDominatingSetSize);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    test_example, FominKratschWoegingerTest, testing::Values(
+        MinimumDominatingSetParameters{
+            32, {{0,1}, {0,2}, {0,4}, {0,8}, {0,16}, {1,2}, {1,3}, {1,5}, {1,9}, {1,17}, {2,3},
+                {2,4}, {2,6}, {2,10}, {2,18}, {3,4}, {3,5}, {3,7}, {3,11}, {3,19}, {4,5}, {4,6},
+                {4,8}, {4,12}, {4,20}, {5,6}, {5,7}, {5,9}, {5,13}, {5,21}, {6,7}, {6,8}, {6,10},
+                {6,14}, {6,22}, {7,8}, {7,9}, {7,11}, {7,15}, {7,23}, {8,9}, {8,10}, {8,12}, {8,16},
+                {8,24}, {9,10}, {9,11}, {9,13}, {9,17}, {9,25}, {10,11}, {10,12}, {10,14}, {10,18},
+                {10,26}, {11,12}, {11,13}, {11,15}, {11,19}, {11,27}, {12,13}, {12,14}, {12,16},
+                {12,20}, {12,28}, {13,14}, {13,15}, {13,17}, {13,21}, {13,29}, {14,15}, {14,16},
+                {14,18}, {14,22}, {14,30}, {15,16}, {15,17}, {15,19}, {15,23}, {15,31}, {16,17},
+                {16,18}, {16,20}, {16,24}, {17,18}, {17,19}, {17,21}, {17,25}, {18,19}, {18,20},
+                {18,22}, {18,26}, {19,20}, {19,21}, {19,23}, {19,27}, {20,21}, {20,22}, {20,24},
+                {20,28}, {21,22}, {21,23}, {21,25}, {21,29}, {22,23}, {22,24}, {22,26}, {22,30},
+                {23,24}, {23,25}, {23,27}, {23,31}, {24,25}, {24,26}, {24,28}, {25,26}, {25,27},
+                {25,29}, {26,27}, {26,28}, {26,30}, {27,28}, {27,29}, {27,31}, {28,29}, {28,30},
+                {29,30}, {29,31}, {30,31}}, 5}
+    )
+);
+
+TEST_P(SchiermeyerTest, test) {
+    MinimumDominatingSetParameters const& parameters = GetParam();
+    NetworKit::Graph G = build_graph(parameters.N, parameters.E);
+    auto algorithm = SchiermeyerMDS(G);
+    algorithm.run();
+    EXPECT_TRUE(algorithm.isDominating(algorithm.getDominatingSet()));
+    EXPECT_EQ(
+        MinimumDominatingSet::dominatingSetSize(algorithm.getDominatingSet()),
+        parameters.minimumDominatingSetSize);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    test_example, SchiermeyerTest, testing::Values(
+        MinimumDominatingSetParameters{
+            32, {{0,1}, {0,2}, {0,4}, {0,8}, {0,16}, {1,2}, {1,3}, {1,5}, {1,9}, {1,17}, {2,3},
+                {2,4}, {2,6}, {2,10}, {2,18}, {3,4}, {3,5}, {3,7}, {3,11}, {3,19}, {4,5}, {4,6},
+                {4,8}, {4,12}, {4,20}, {5,6}, {5,7}, {5,9}, {5,13}, {5,21}, {6,7}, {6,8}, {6,10},
+                {6,14}, {6,22}, {7,8}, {7,9}, {7,11}, {7,15}, {7,23}, {8,9}, {8,10}, {8,12}, {8,16},
+                {8,24}, {9,10}, {9,11}, {9,13}, {9,17}, {9,25}, {10,11}, {10,12}, {10,14}, {10,18},
+                {10,26}, {11,12}, {11,13}, {11,15}, {11,19}, {11,27}, {12,13}, {12,14}, {12,16},
+                {12,20}, {12,28}, {13,14}, {13,15}, {13,17}, {13,21}, {13,29}, {14,15}, {14,16},
+                {14,18}, {14,22}, {14,30}, {15,16}, {15,17}, {15,19}, {15,23}, {15,31}, {16,17},
+                {16,18}, {16,20}, {16,24}, {17,18}, {17,19}, {17,21}, {17,25}, {18,19}, {18,20},
+                {18,22}, {18,26}, {19,20}, {19,21}, {19,23}, {19,27}, {20,21}, {20,22}, {20,24},
+                {20,28}, {21,22}, {21,23}, {21,25}, {21,29}, {22,23}, {22,24}, {22,26}, {22,30},
+                {23,24}, {23,25}, {23,27}, {23,31}, {24,25}, {24,26}, {24,28}, {25,26}, {25,27},
+                {25,29}, {26,27}, {26,28}, {26,30}, {27,28}, {27,29}, {27,31}, {28,29}, {28,30},
+                {29,30}, {29,31}, {30,31}}, 5}
+    )
+);
+
+TEST_P(ExhaustiveTest, test) {
+    MinimumDominatingSetParameters const& parameters = GetParam();
+    NetworKit::Graph G = build_graph(parameters.N, parameters.E);
+    auto algorithm = ExhaustiveMDS(G);
+    algorithm.run();
+    EXPECT_TRUE(algorithm.isDominating(algorithm.getDominatingSet()));
+    EXPECT_EQ(
+        MinimumDominatingSet::dominatingSetSize(algorithm.getDominatingSet()),
+        parameters.minimumDominatingSetSize);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    test_example, ExhaustiveTest, testing::Values(
+        MinimumDominatingSetParameters{
+            32, {{0,1}, {0,2}, {0,4}, {0,8}, {0,16}, {1,2}, {1,3}, {1,5}, {1,9}, {1,17}, {2,3},
+                {2,4}, {2,6}, {2,10}, {2,18}, {3,4}, {3,5}, {3,7}, {3,11}, {3,19}, {4,5}, {4,6},
+                {4,8}, {4,12}, {4,20}, {5,6}, {5,7}, {5,9}, {5,13}, {5,21}, {6,7}, {6,8}, {6,10},
+                {6,14}, {6,22}, {7,8}, {7,9}, {7,11}, {7,15}, {7,23}, {8,9}, {8,10}, {8,12}, {8,16},
+                {8,24}, {9,10}, {9,11}, {9,13}, {9,17}, {9,25}, {10,11}, {10,12}, {10,14}, {10,18},
+                {10,26}, {11,12}, {11,13}, {11,15}, {11,19}, {11,27}, {12,13}, {12,14}, {12,16},
+                {12,20}, {12,28}, {13,14}, {13,15}, {13,17}, {13,21}, {13,29}, {14,15}, {14,16},
+                {14,18}, {14,22}, {14,30}, {15,16}, {15,17}, {15,19}, {15,23}, {15,31}, {16,17},
+                {16,18}, {16,20}, {16,24}, {17,18}, {17,19}, {17,21}, {17,25}, {18,19}, {18,20},
+                {18,22}, {18,26}, {19,20}, {19,21}, {19,23}, {19,27}, {20,21}, {20,22}, {20,24},
+                {20,28}, {21,22}, {21,23}, {21,25}, {21,29}, {22,23}, {22,24}, {22,26}, {22,30},
+                {23,24}, {23,25}, {23,27}, {23,31}, {24,25}, {24,26}, {24,28}, {25,26}, {25,27},
+                {25,29}, {26,27}, {26,28}, {26,30}, {27,28}, {27,29}, {27,31}, {28,29}, {28,30},
+                {29,30}, {29,31}, {30,31}}, 5}
+    )
+);
