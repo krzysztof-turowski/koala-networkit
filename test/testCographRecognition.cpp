17963706f51060a4a292f79fab80ea277bc8c95f
milana <milana.kananovich@student.uj.edu.pl>
2024-03-06 16:12:09 +0100

    add

--- test/testCographRecognition.cpp
+++ test/testCographRecognition.cpp
@@ -3,9 +3,10 @@
 //
 #include <gtest/gtest.h>
 
+
 #include <list>
 
-#include <recognition/PerfectGraphRecognition.hpp>
+#include <cograph_recognition/CographRecognition.hpp>
 
 #include "helpers.hpp"
 
@@ -15,21 +16,21 @@ struct GraphRecognitionParameters {
     bool is_recognized;
 };
 
-class PerfectGraphRecognitionTest
+class CographRecognitionTest
         : public testing::TestWithParam<GraphRecognitionParameters> { };
 
-TEST_P(PerfectGraphRecognitionTest, test) {
+TEST_P(CographRecognitionTest, test) {
 GraphRecognitionParameters const& parameters = GetParam();
 NetworKit::Graph G = build_graph(parameters.N, parameters.E, false);
-auto algorithm = Koala::PerfectGraphRecognition(G);
+auto algorithm = Koala::CographRecognition(G);
 algorithm.run();
 
-auto is_perfect = algorithm.isPerfect();
+auto is_perfect = algorithm.isComplementReducible();
 EXPECT_EQ(is_perfect, parameters.is_recognized);
 }
 
 INSTANTIATE_TEST_SUITE_P(
-        test_example, PerfectGraphRecognitionTest, testing::Values(
-        GraphRecognitionParameters{4, {{0, 1}, {0, 2}, {1, 3}, {2, 3}}, true},
-        GraphRecognitionParameters{5, {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 0}}, false}
+        test_example, CographRecognitionTest, testing::Values(
+        GraphRecognitionParameters{5, {{0, 1}, {1, 2}, {2, 3}, {3, 4}}, false},
+        GraphRecognitionParameters{5, {{0, 1}, {1, 2},  {3, 4}}, true}
 ));
