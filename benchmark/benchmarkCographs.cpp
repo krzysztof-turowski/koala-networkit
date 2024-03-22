27995ae1e067bf6126d1b613135acca3fa536994
milana <milana.kananovich@student.uj.edu.pl>
2024-03-22 11:15:29 +0100

    fixed strange behaviour

--- benchmark/benchmarkCographs.cpp
+++ benchmark/benchmarkCographs.cpp
@@ -5,10 +5,15 @@
 #include <iostream>
 #include <map>
 
-#include <io/G6GraphReader.hpp>
-#include <cograph_recognition/CographRecognition.hpp>
+#include "io/G6GraphReader.hpp"
+#include "cograph_recognition/CographRecognition.hpp"
+#include <fstream>
+#include <filesystem>
 
+namespace fs = std::filesystem;
+using namespace std;
 int main() {
+    std::ifstream fin("../../input/cographConnected14.g6");
     std::map<Koala::CographRecognition::State, int> classification;
     std::string types[] = {
             "UNKNOWN",
@@ -20,18 +25,25 @@ int main() {
             "COND_5",
             "COND_6"
     };
-
-    while (true) {
+    cout<<"why?"<<endl;
+    std::cout << "Current path is " << fs::current_path() <<" "<<fin.is_open()<<std::endl;
+    int cnt = 0;
+    while (cnt !=2 ) {
+        cnt++;
         std::string line;
-        std::cin >> line;
+        fin >> line;
         if (!std::cin.good()) {
             break;
         }
         NetworKit::Graph G = Koala::G6GraphReader().readline(line);
         auto recognize = Koala::CographRecognition(G);
+        cout<<"aaa"<<endl;
         recognize.run();
+        cout<<"bbb"<<endl;
         classification[recognize.getState()]++;
+        cout<<"ccc"<<endl;
         recognize.check();
+        cout<<"ddd"<<endl;
     }
     for (const auto &[k, v] : classification) {
         std::cout << types[static_cast<int>(k)] << ": " << v << std::endl;
