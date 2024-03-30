02afc1edff25aabdb92d14a85a3cbc9f1228f591
milana <milana.kananovich@student.uj.edu.pl>
2024-03-30 15:02:23 +0100

    fixing issues

--- benchmark/benchmarkCographs.cpp
+++ benchmark/benchmarkCographs.cpp
@@ -12,105 +12,52 @@
 
 namespace fs = std::filesystem;
 using namespace std;
+std::string types[] = {
+        "UNKNOWN",
+        "COMPLEMENT_REDUCIBLE",
+        "COND_1",
+        "COND_2",
+        "COND_3",
+        "COND_4",
+        "COND_5",
+        "COND_6"
+};
+pair<int,int> test(int j, string s, string s1){
+    string s2 = "";
+    if(j >= 10)s2 += "1";
+    s2 += '0' + j % 10;
+    std::ifstream fin(s + s2 + s1);
+    std::map<Koala::CographRecognition::State, int> classification;
+    int cnt = 0;
+    while (true) {
+        std::string line;
+        fin >> line;
+        if (!fin.good()) {
+            break;
+        }
+        cnt++;
+        NetworKit::Graph G = Koala::G6GraphReader().readline(line);
+        auto recognize = Koala::CographRecognition(G);
+        recognize.run();
+        classification[recognize.getState()]++;
+    }
+    cout <<"size: "<<j<< " total: "<<cnt<< endl;
+    for (const auto &[k, v]: classification) {
+        std::cout << types[static_cast<int>(k)] << ": " << v << std::endl;
+    }
+    return {cnt,classification[Koala::CographRecognition::State::COMPLEMENT_REDUCIBLE]};
+}
 int main() {
-    std::string types[] = {
-            "UNKNOWN",
-            "COMPLEMENT_REDUCIBLE",
-            "COND_1",
-            "COND_2",
-            "COND_3",
-            "COND_4",
-            "COND_5",
-            "COND_6"
-    };
+
     for(int j = 3; j <= 9; j++){
-        string s = "../../input/cographConnected", s1 = ".g6";
-        string s2 = "";
-        if(j >= 10)s2 += "1";
-        s2 += '0' + j % 10;
-        std::ifstream fin(s + s2 + s1);
-        std::map<Koala::CographRecognition::State, int> classification;
-        int cnt = 0;
-        while (true) {
 
-
-            std::string line;
-            fin >> line;
-            if (!fin.good()) {
-                break;
-            }
-            cnt++;
-            NetworKit::Graph G = Koala::G6GraphReader().readline(line);
-            auto recognize = Koala::CographRecognition(G);
-            recognize.run();
-            classification[recognize.getState()]++;
-            recognize.check();
-            if (recognize.getState() != Koala::CographRecognition::State::COMPLEMENT_REDUCIBLE)cout << cnt << endl;
-        }
-        cout <<"size: "<<j<< " total: "<<cnt<< endl;
-        for (const auto &[k, v]: classification) {
-            std::cout << types[static_cast<int>(k)] << ": " << v << std::endl;
-        }
-
-
-
-        s = "../../input/graph", s1 = "c.g6";
-        s2 = "";
-        if(j >= 10)s2 += "1";
-        s2 += '0' + j % 10;
-        std::ifstream din(s + s2 + s1);
-        std::map<Koala::CographRecognition::State, int> classification1;
-        int cnt1 = 0;
-        while (true) {
-
-
-            std::string line;
-            din >> line;
-            if (!din.good()) {
-                break;
-            }
-            cnt1++;
-            NetworKit::Graph G = Koala::G6GraphReader().readline(line);
-            auto recognize = Koala::CographRecognition(G);
-            recognize.run();
-            classification1[recognize.getState()]++;
-            recognize.check();
-        }
-        cout <<"size: "<<j<< " total: "<<cnt1<< endl;
-        for (const auto &[k, v]: classification1) {
-            std::cout << types[static_cast<int>(k)] << ": " << v << std::endl;
-        }
-        if(cnt == classification[Koala::CographRecognition::State::COMPLEMENT_REDUCIBLE] && classification1[Koala::CographRecognition::State::COMPLEMENT_REDUCIBLE] == cnt)cout<<"OK"<<endl;
+        pair<int,int> cnt = test(j, "../../input/cographConnected", ".g6");
+        pair<int,int> cnt1 = test(j, "../../input/graph","c.g6");
+        if(cnt.first == cnt.second && cnt1.second == cnt.first)cout<<"OK"<<endl;
         else cout<<"FAIL"<<endl;
     }
     for(int j = 3; j <= 15; j++) {
-        string s = "../../input/cographConnected", s1 = ".g6";
-        string s2 = "";
-        if(j >= 10)s2 += "1";
-        s2 += '0' + j % 10;
-        std::ifstream fin(s + s2 + s1);
-        std::map<Koala::CographRecognition::State, int> classification;
-        int cnt = 0;
-        while (true) {
-
-
-            std::string line;
-            fin >> line;
-            if (!fin.good()) {
-                break;
-            }
-            cnt++;
-            NetworKit::Graph G = Koala::G6GraphReader().readline(line);
-            auto recognize = Koala::CographRecognition(G);
-            recognize.run();
-            classification[recognize.getState()]++;
-            recognize.check();
-            if (recognize.getState() != Koala::CographRecognition::State::COMPLEMENT_REDUCIBLE)cout << cnt << endl;
-        }
-        cout <<"size: "<<j<< " total: "<<cnt<< endl;
-        for (const auto &[k, v]: classification) {
-            std::cout << types[static_cast<int>(k)] << ": " << v << std::endl;
-        }
+        test(j,"../../input/cographConnected", ".g6");
     }
     return 0;
 }
