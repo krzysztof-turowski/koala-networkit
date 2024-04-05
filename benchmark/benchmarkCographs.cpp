6d6a6f84b711b23a7da0ad1d116495c130443b4f
milana <milana.kananovich@student.uj.edu.pl>
2024-04-05 19:30:43 +0200

    fixed benchmark

--- benchmark/benchmarkCographs.cpp
+++ benchmark/benchmarkCographs.cpp
@@ -5,76 +5,32 @@
 #include <map>
 #include "io/G6GraphReader.hpp"
 #include "cograph_recognition/CorneilStewartPerlCographRecognition.h"
-#include <fstream>
-std::string types[] = {
-        "UNKNOWN",
-        "COGRAPH",
-        "CONTAINS_0_NODE",
-        "EXISTS_1_NODE_NOT_PROPERLY_MARKED",
-        "GRANDPARENT_IS_NOT_IN_SET",
-        "NO_ONE_PATH",
-        "WRONG_PARENT",
-        "WRONG_GRANDPARENT"
-};
-std::pair<int,int> test(int j, const std::string& s, const std::string& s1){
-    std::string s2;
-    if(j >= 10)s2 += "1";
-    s2 += '0' + j % 10;
-    std::ifstream fin(s + s2 + s1);
-    std::map<Koala::CorneilStewartPerlCographRecognition::State, int> classification;
-    int cnt = 0;
-    while (true) {
-        std::string line;
-        fin >> line;
-        if (!fin.good()) {
-            break;
-        }
-        cnt++;
-        NetworKit::Graph G = Koala::G6GraphReader().readline(line);
-        auto recognize = Koala::CorneilStewartPerlCographRecognition(G);
-        recognize.run();
-        classification[recognize.getState()]++;
-    }
-    std::cout <<"size: "<<j<< " total: "<<cnt<< std::endl;
-    for (const auto &[k, v]: classification) {
-        std::cout << types[static_cast<int>(k)] << ": " << v << std::endl;
-    }
-    return {cnt,classification[Koala::CorneilStewartPerlCographRecognition::State::COGRAPH]};
-}
-void auto_test(){
-    for(int j = 3; j <= 9; j++){
 
-        std::pair<int,int> cnt = test(j, "../../input/cographConnected", ".g6");
-        std::pair<int,int> cnt1 = test(j, "../../input/graph","c.g6");
-        if(cnt.first == cnt.second && cnt1.second == cnt.first)std::cout<<"OK"<<std::endl;
-        else std::cout<<"FAIL"<<std::endl;
-    }
-    for(int j = 3; j <= 15; j++) {
-        test(j,"../../input/cographConnected", ".g6");
-    }
-}
-void manual_test(){
+int main() {
     std::map<Koala::CorneilStewartPerlCographRecognition::State, int> classification;
-    int cnt = 0;
+    std::string types[] = {
+            "UNKNOWN",
+            "COGRAPH",
+            "CONTAINS_0_NODE",
+            "EXISTS_1_NODE_NOT_PROPERLY_MARKED",
+            "GRANDPARENT_IS_NOT_IN_SET",
+            "NO_ONE_PATH",
+            "WRONG_PARENT",
+            "WRONG_GRANDPARENT"
+    };
     while (true) {
         std::string line;
         std::cin >> line;
         if (!std::cin.good()) {
             break;
         }
-        cnt++;
         NetworKit::Graph G = Koala::G6GraphReader().readline(line);
         auto recognize = Koala::CorneilStewartPerlCographRecognition(G);
         recognize.run();
         classification[recognize.getState()]++;
     }
-    std::cout << " total: "<<cnt<< std::endl;
     for (const auto &[k, v]: classification) {
         std::cout << types[static_cast<int>(k)] << ": " << v << std::endl;
     }
-}
-int main() {
-    ///auto_test();//to check the correctness of algorithm
-    manual_test();
     return 0;
 }
