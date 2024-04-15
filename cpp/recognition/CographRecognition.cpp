b66641f71b6457c59a12d4771ea867732526738f
milana <milana.kananovich@student.uj.edu.pl>
2024-04-15 11:39:25 +0200

    cleanup

--- cpp/recognition/CographRecognition.cpp
+++ cpp/recognition/CographRecognition.cpp
@@ -32,25 +32,20 @@ namespace Koala {
         }
         is_cograph = State::COGRAPH;
     }
-
+    bool checkPath(const NetworKit::Graph &graph, NetworKit::node x, NetworKit::node y, NetworKit::node u, NetworKit::node v){
+        return graph.hasEdge(y, u) && !graph.hasEdge(x, u) && !graph.hasEdge(x, v) && !graph.hasEdge(y, v);
+    }
     void CorneilStewartPerlCographRecognition::check() const {
         assureFinished();
-        bool is_cograph = true;
         for (auto e1: graph.edgeRange()) {
             for (auto e2: graph.edgeRange()) {
-                auto x = e1.u;
-                auto y = e1.v;
-                auto u = e2.u;
-                auto v = e2.v;
+                auto [x, y] = e1;
+                auto [u, v] = e2;
                 if (x == u || x == v || y == u || y == v) {
                     continue;
                 }
-                if (graph.hasEdge(y, u) && !graph.hasEdge(x, u) && !graph.hasEdge(x, v) && !graph.hasEdge(y, v)) {
-                    is_cograph = false;
-                    break;
-                }
+                assert(!checkPath(graph, x, y, u, v) && !checkPath(graph, x, y, v, u) && !checkPath(graph, y, x, u, v) && !checkPath(graph, y, x, v, u));
             }
         }
-        assert(is_cograph);
     }
 }  // namespace Koala
