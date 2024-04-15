dd81f00740a5ec22ddce668ad1eb41013a69e3d5
milana <milana.kananovich@student.uj.edu.pl>
2024-04-15 15:11:50 +0200

    cleanup

--- cpp/recognition/cograph/CoTree.cpp
+++ cpp/recognition/cograph/CoTree.cpp
@@ -1,5 +1,6 @@
 //
 // Created by milana on 30.03.24.
+// Copyright 2024 milana
 //
 #include <list>
 #include <graph/GraphTools.hpp>
@@ -14,25 +15,25 @@ namespace Koala {
         MARKED,
         MARKED_AND_UNMARKED
     };
-
-    class CoNode {
-    public:
+class CoNode{
+ public:
         Type type;
         int number;
         Marked marked;
-        //d is the current number of children
-        //md is the current number of children, which have been both "marked" and "unmarked"
+        // d is the current number of children
+        // md is the current number of children,
+        // which have been both "marked" and "unmarked"
         int md, d;
         bool in_graph;
         CoNode *first_child;
-        CoNode *next, *previous;//in list of children of its parent
+        CoNode *next, *previous;  // in list of children of its parent
         CoNode *parent;
-        std::vector<CoNode*> out_edges;//neighbours of cur vertex in G
+        std::vector<CoNode*> out_edges;  // neighbours of cur vertex in G
 
-        CoNode(Type type, int number) : type(type), number(number), marked(Marked::UNMARKED), md(0), d(0),
-                                        in_graph(false), first_child(nullptr),
-                                        next(nullptr), previous(nullptr), parent(nullptr) {
-
+        CoNode(Type type, int number) : type(type), number(number),
+            marked(Marked::UNMARKED), md(0), d(0), in_graph(false),
+            first_child(nullptr), next(nullptr), previous(nullptr),
+            parent(nullptr) {
         }
 
         void AddChild(CoNode *x) {
@@ -103,12 +104,12 @@ namespace Koala {
                 u = next;
             }
         }
-    };
+};
 
-    class CoTree {
-    private:
+class CoTree {
+ private:
         std::vector<CoNode*> save;
-    public:
+ public:
         CoNode *root;
 
         CoNode* Add(Type type, int number) {
@@ -118,10 +119,10 @@ namespace Koala {
         }
 
         void Clear() {
-            for (auto u: save) {
+            for (auto u : save) {
                 delete u;
             }
             save.clear();
         }
-    };
-}
+};
+}  // namespace Koala
--- cpp/recognition/cograph/CorneilStewartPerlCographRecognition.cpp
+++ cpp/recognition/cograph/CorneilStewartPerlCographRecognition.cpp
@@ -1,3 +1,5 @@
+// Copyright 2024 milana
+
 #include <list>
 #include <graph/GraphTools.hpp>
 #include "recognition/CorneilStewartPerlCographRecognition.hpp"
