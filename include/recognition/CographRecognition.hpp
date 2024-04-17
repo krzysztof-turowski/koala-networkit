eb236c012adbc0152a5ff151a40c7cf0ff09a2f8
milana <milana.kananovich@student.uj.edu.pl>
2024-04-17 21:46:40 +0200

    ingroup add

--- include/recognition/CographRecognition.hpp
+++ include/recognition/CographRecognition.hpp
@@ -44,41 +44,47 @@ class CographRecognition : public NetworKit::Algorithm {
     NetworKit::Graph graph;
 };
 
-    class CorneilStewartPerlCographRecognition : public CographRecognition {
-    public:
-        using CographRecognition::CographRecognition;
-        enum class State {
-            UNKNOWN,
-            COGRAPH,
-            CONTAINS_0_NODE,
-            EXISTS_1_NODE_NOT_PROPERLY_MARKED,
-            GRANDPARENT_IS_NOT_IN_SET,
-            NO_ONE_PATH,
-            WRONG_PARENT,
-            WRONG_GRANDPARENT
-        };
-        /**
-         * Return the graph type found by the algorithm.
-         *
-         * @return State of the graph.
-         */
-        State getState() const;
-        CorneilStewartPerlCographRecognition::State Recognition();
-        void run() override;
-        bool isCograph() const override;
-        void Unmark();
-        void Mark(CoNode *x);
-        std::pair<CoNode*, CorneilStewartPerlCographRecognition::State>FindLowest();
-        void InsertXToCoTree(CoNode *u, CoNode *x);
-
-    private:
-        State is_cograph = State::UNKNOWN;
-        CoTree T;
-        NetworKit::Graph G;
-        int mark_count = 0;
-        int mark_and_unmarked_count = 0;
-        int mark_ever_count = 0;
-        std::queue<CoNode*> marked_with_d_equal_to_md;  // TODO: get rid of this
+/**
+ * @ingroup recognition
+ * The class for recognition of cographs procedure from
+ * Corneil, Stewart, Perl, "A linear recognition algorithm for cographs".
+ *
+ */
+class CorneilStewartPerlCographRecognition : public CographRecognition {
+ public:
+    using CographRecognition::CographRecognition;
+    enum class State {
+        UNKNOWN,
+        COGRAPH,
+        CONTAINS_0_NODE,
+        EXISTS_1_NODE_NOT_PROPERLY_MARKED,
+        GRANDPARENT_IS_NOT_IN_SET,
+        NO_ONE_PATH,
+        WRONG_PARENT,
+        WRONG_GRANDPARENT
     };
+    /**
+     * Return the graph type found by the algorithm.
+     *
+     * @return State of the graph.
+     */
+    State getState() const;
+    CorneilStewartPerlCographRecognition::State Recognition();
+    void run() override;
+    bool isCograph() const override;
+    void Unmark();
+    void Mark(CoNode *x);
+    std::pair<CoNode*, CorneilStewartPerlCographRecognition::State>FindLowest();
+    void InsertXToCoTree(CoNode *u, CoNode *x);
+
+ private:
+    State is_cograph = State::UNKNOWN;
+    CoTree T;
+    NetworKit::Graph G;
+    int mark_count = 0;
+    int mark_and_unmarked_count = 0;
+    int mark_ever_count = 0;
+    std::queue<CoNode*> marked_with_d_equal_to_md;  // TODO: get rid of this
+};
 
 } /* namespace Koala */
