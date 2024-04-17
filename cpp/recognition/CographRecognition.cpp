dbdcb7df2508d01a34a1a36035fc54684265ca36
Krzysztof Turowski <krzysztof.szymon.turowski@gmail.com>
2024-04-17 18:30:49 +0200

    Fix a few style issues

--- cpp/recognition/CographRecognition.cpp
+++ cpp/recognition/CographRecognition.cpp
@@ -12,29 +12,27 @@
 
 namespace Koala {
 
-    CographRecognition::CographRecognition(const NetworKit::Graph &graph)
-    : graph(graph) {}
+CographRecognition::CographRecognition(const NetworKit::Graph &graph) : graph(graph) {}
 
-    bool checkPath(const NetworKit::Graph &graph,
-                   NetworKit::node x, NetworKit::node y,
-                   NetworKit::node u, NetworKit::node v) {
-        return graph.hasEdge(y, u) && !graph.hasEdge(x, u)
+bool checkPath(
+        const NetworKit::Graph &graph, NetworKit::node x, NetworKit::node y,
+        NetworKit::node u, NetworKit::node v) {
+    return graph.hasEdge(y, u) && !graph.hasEdge(x, u)
         && !graph.hasEdge(x, v) && !graph.hasEdge(y, v);
-    }
+}
 
-    void CographRecognition::check() const {
-        assureFinished();
-        for (const auto [x, y] : graph.edgeRange()) {
-            for (const auto [u, v] : graph.edgeRange()) {
-                if (x == u || x == v || y == u || y == v) {
-                    continue;
-                }
-                assert(!checkPath(graph, x, y, u, v) &&
-                !checkPath(graph, x, y, v, u)&&
-                !checkPath(graph, y, x, u, v) &&
-                !checkPath(graph, y, x, v, u));
+void CographRecognition::check() const {
+    assureFinished();
+    for (const auto &[x, y] : graph.edgeRange()) {
+        for (const auto &[u, v] : graph.edgeRange()) {
+            if (x == u || x == v || y == u || y == v) {
+                continue;
             }
+            assert(
+                !checkPath(graph, x, y, u, v) && !checkPath(graph, x, y, v, u)
+                && !checkPath(graph, y, x, u, v) && !checkPath(graph, y, x, v, u));
         }
     }
+}
 
 }  // namespace Koala
