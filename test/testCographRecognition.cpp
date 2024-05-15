343311aaec98623633ba4b6c71e2528066bd5347
fixikmila <99081680+fixikmila@users.noreply.github.com>
2024-05-15 17:00:29 +0200

    Update testCographRecognition.cpp

--- test/testCographRecognition.cpp
+++ test/testCographRecognition.cpp
@@ -20,18 +20,49 @@ class CographRecognitionTest
         : public testing::TestWithParam<GraphRecognitionParameters> {
 };
 
+class CographLexBfsRecognitionTest
+        : public testing::TestWithParam<GraphRecognitionParameters> {
+};
+
 TEST_P(CographRecognitionTest, test) {
     GraphRecognitionParameters const &parameters = GetParam();
     NetworKit::Graph G = build_graph(parameters.N, parameters.E, false);
     auto algorithm = Koala::CorneilStewartPerlCographRecognition(G);
     algorithm.run();
 
-    auto is_perfect = algorithm.isCograph();
-    EXPECT_EQ(is_perfect, parameters.is_recognized);
+    auto is_cograph = algorithm.isCograph();
+    EXPECT_EQ(is_cograph, parameters.is_recognized);
 }
 
 INSTANTIATE_TEST_SUITE_P(
         test_example, CographRecognitionTest, testing::Values(
+        GraphRecognitionParameters{4, {{0, 2}, {0, 3}, {1, 3}, {2, 3}}, true},
+        GraphRecognitionParameters{5, {{0, 3}, {0, 4}, {3, 4}, {1, 3}, {2, 4}}, false},
+        GraphRecognitionParameters{3, {{0, 1}, {0, 2}}, true},
+        GraphRecognitionParameters{5, {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {2, 4}}, false},
+        GraphRecognitionParameters{5, {{0, 1}, {1, 2}, {3, 4}}, true},
+        GraphRecognitionParameters{5, {{0, 1}, {0, 2}, {0, 3}, {1, 3}, {3, 4}}, false},
+        GraphRecognitionParameters{5, {{0, 1}, {1, 2}, {2, 3}, {1, 4}, {2, 4}}, false},
+        GraphRecognitionParameters{7, {{0, 1}, {1, 2}, {0, 2}, {1, 4}, {2, 4}, {3, 2}, {3, 4},
+                                       {4, 5}, {5, 6}, {2, 6}}, false},
+        GraphRecognitionParameters{7, {{0, 1}, {1, 2}, {0, 2}, {1, 4}, {2, 4}, {3, 2}, {3, 4},
+                                       {4, 5}, {5, 6}}, false}
+));
+
+TEST_P(CographLexBfsRecognitionTest, test) {
+    GraphRecognitionParameters const &parameters = GetParam();
+    NetworKit::Graph G = build_graph(parameters.N, parameters.E, false);
+    auto algorithm = Koala::BretscherCorneilHabibPaulCographRecognition(G);
+    algorithm.run();
+
+    auto is_cograph = algorithm.isCograph();
+    EXPECT_EQ(is_cograph, parameters.is_recognized);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+        test_example, CographLexBfsRecognitionTest, testing::Values(
+        GraphRecognitionParameters{4, {{0, 2}, {0, 3}, {1, 3}, {2, 3}}, true},
+        GraphRecognitionParameters{5, {{0, 3}, {0, 4}, {3, 4}, {1, 3}, {2, 4}}, false},
         GraphRecognitionParameters{3, {{0, 1}, {0, 2}}, true},
         GraphRecognitionParameters{5, {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {2, 4}}, false},
         GraphRecognitionParameters{5, {{0, 1}, {1, 2}, {3, 4}}, true},
