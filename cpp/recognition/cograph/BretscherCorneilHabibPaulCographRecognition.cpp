a421ea5a659ebed02e0aad66517c01970e87103d
fixikmila <99081680+fixikmila@users.noreply.github.com>
2024-05-15 16:16:23 +0200

    done lexbfs cograph recognitipon

--- cpp/recognition/cograph/BretscherCorneilHabibPaulCographRecognition.cpp
+++ cpp/recognition/cograph/BretscherCorneilHabibPaulCographRecognition.cpp
@@ -14,63 +14,199 @@
 
 namespace Koala {
 
-bool BretscherCorneilHabibPaulCographRecognition::isCograph() const {
-    assureFinished();
-    return is_cograph;
-}
-
-void BretscherCorneilHabibPaulCographRecognition::run() {
-    hasRun = true;
-    auto a = LexBfs();
-    auto b = LexBfsMinus(true, graph, a);//graph'
-    auto c = LexBfsMinus(false, graph, b);
-    is_cograph = NeighbourhoodSubsetProperty(b, c);
-}
-
-std::vector<NetworKit::node> BretscherCorneilHabibPaulCographRecognition::LexBfs(){
-    std::vector<NetworKit::node> a(graph.numberOfNodes());
-    std::list<std::list<NetworKit::node>> L;
-    std::list<NetworKit::node> first;
-    for(auto i : graph.nodeRange()) {
-        first.push_back(i);
+    bool BretscherCorneilHabibPaulCographRecognition::isCograph() const {
+        assureFinished();
+        return is_cograph;
     }
-    L.push_back(first);
-    int i = 0;
-    while(!L.empty()) {
-        auto &l = L.front();
-        auto &x = l.front();
-        l.pop_front();
-        a[x] = i++;
-        for(auto it = L.begin(); it != L.end(); it++) {
-            auto &j = *it;
-            std::list<NetworKit::node> P;
-            for(auto u = j.begin(); u != j.end(); u++) {
-                if(graph.hasEdge(*u, x)) {
-                    P.push_back(*u);
-                    j.erase(u);
-                }
-            }
-            if(j.empty()){
-                for(auto u : P) {
-                    j.push_back(u);
-                }
-            }
-            if(!P.empty()) {
-                L.insert(it,P);
-                it++;
-            }
+
+    void BretscherCorneilHabibPaulCographRecognition::run() {
+        hasRun = true;
+        std::vector<NetworKit::node> start;
+        for (auto u : graph.nodeRange()) {
+            start.push_back(u);
+        }
+        auto [bb, a] = LexBfsMinus(false, start);
+        auto [borders1, b] = LexBfsMinus(true, a);  // graph complement
+        auto [borders2, c] = LexBfsMinus(false, b);
+        if (NeighbourhoodSubsetProperty(true, b, borders1) &&
+        NeighbourhoodSubsetProperty(false, c, borders2)) {
+            is_cograph = true;
+        } else {
+            is_cograph = false;
         }
     }
-    return a;
-}
 
-std::vector<NetworKit::node> BretscherCorneilHabibPaulCographRecognition::
-LexBfsMinus(bool is_complement, NetworKit::Graph G, std::vector<NetworKit::node> a){
+    std::pair<std::vector<std::vector<std::pair<int, int>>>, std::vector<NetworKit::node>>
+    BretscherCorneilHabibPaulCographRecognition::
+    LexBfsMinus(bool is_complement, std::vector<NetworKit::node> &a) {
+        std::vector<NetworKit::node> ans(a.size(), INT_MAX);
+        std::vector<bool> used(a.size());
+        std::vector<std::vector<std::pair<NetworKit::node, unsigned int>>> what_ends_here(a.size());
+        std::vector<std::vector<std::pair<int, int>>> borders(a.size());
+        std::list<std::list<NetworKit::node>> L;
+        std::list<NetworKit::node> first;
+        for (auto i : a) {
+            first.push_back(i);
+        }
+        L.push_back(first);
+        std::vector<std::_List_iterator<std::list<NetworKit::node>>>
+        in_which_list(a.size(), L.begin()), previous_list(a.size(), L.begin());
+        std::vector<std::list<NetworKit::node>::iterator> in_which_position(a.size());
+        std::vector<bool> used_at_this_step(a.size());
+        auto it = L.front().begin();
+        for (unsigned int i : a) {
+            in_which_position[i] = it++;
+        }
+        int i = 0;
+        while (!L.empty()) {
+            for (auto &[x, sz] : what_ends_here[i]) {
+                unsigned int current_sum = 0;
+                for (const auto &l : L) {
+                    if (l.size() + current_sum <= sz) {
+                        borders[x].emplace_back(i + current_sum, i + current_sum + l.size() - 1);
+                        current_sum += l.size();
+                    } else {
+                        break;
+                    }
+                }
+            }
 
-}
+            auto x = L.front().front();
+            auto slice_size = L.front().size();
+            L.front().pop_front();
+            if (L.front().empty()) {
+                L.erase(L.begin());
+            }
+            ans[i++] = x;
+            used[x] = true;
+            unsigned int SA = 0;
+            for (auto v : graph.neighborRange(x)) {
+                used_at_this_step[v] = true;
+                previous_list[v] = in_which_list[v];
+                if (used[v]) {
+                    continue;
+                }
+                auto number = in_which_list[v];
+                if (slice_size != 1 && number == in_which_list[x]) {
+                    SA++;
+                }
+                auto previous = number;
 
-bool BretscherCorneilHabibPaulCographRecognition::NeighbourhoodSubsetProperty(std::vector<NetworKit::node> a, std::vector<NetworKit::node> b){
+                auto copy_number = number;
+                auto end_it = L.end();
+                end_it--;
 
-}
+                if (is_complement) {
+                    copy_number++;
+                }
+
+
+                if ((number == L.begin() && !is_complement) ||
+                (number == end_it && is_complement)) {
+                    std::list<NetworKit::node> insert;
+                    L.insert(copy_number, insert);
+                }
+                if (!is_complement) {
+                    previous--;
+                } else {
+                    previous++;
+                }
+                if (!previous->empty() && (!graph.hasEdge(previous->front(), x)
+                                           || !used_at_this_step[previous->front()] ||
+                                           previous_list[previous->front()] != previous_list[v])) {
+                    std::list<NetworKit::node> insert;
+                    L.insert(copy_number, insert);
+                    previous = number;
+                    if (is_complement) {
+                        previous++;
+                    } else {
+                        previous--;
+                    }
+                }
+                previous->push_back(v);
+                in_which_list[v] = previous;
+                number->erase(in_which_position[v]);
+                if (number->empty()) {
+                    L.erase(number);
+                }
+
+                auto end = previous->end();
+                end--;
+                in_which_position[v] = end;
+            }
+            if (is_complement) {
+                SA = slice_size - 1 - SA;
+            }
+            for (auto v : graph.neighborRange(x)) {
+                used_at_this_step[v] = false;
+            }
+            if (slice_size - SA - 1 != 0) {
+                what_ends_here[i + SA].emplace_back(x, slice_size - SA - 1);
+            }
+        }
+        return {borders, ans};
+    }
+
+    bool BretscherCorneilHabibPaulCographRecognition::NeighbourhoodSubsetProperty
+    (bool is_complement, std::vector<NetworKit::node> a,
+    std::vector<std::vector<std::pair<int, int>>> borders) {
+        std::vector<bool> used(a.size());
+        std::vector<int> pos(a.size());
+        for (unsigned int i = 0; i < a.size(); i++) {
+            pos[a[i]] = i;
+        }
+        for (auto i : a) {
+            if (borders[i].empty()) {
+                continue;
+            }
+            for (unsigned int j = 0; j < borders[i].size() - 1; j++) {
+                auto l = borders[i][j].first;
+                auto r = borders[i][j].second;
+                auto y = a[l];
+                auto p = a[borders[i][j + 1].first];
+                if (is_complement) {
+                    int cnt = 0;
+                    for (auto z : graph.neighborRange(p)) {
+                        if (pos[z] < pos[p]) {
+                            used[z] = true;
+                        }
+                        if (pos[z] >= l && pos[z] <= r) {
+                            cnt++;
+                        }
+                    }
+                    if (cnt != r - l + 1) {
+                        return false;
+                    }
+                    for (auto z : graph.neighborRange(y)) {
+                        if (pos[z] < pos[y] && !used[z]) {
+                            return false;
+                        }
+                    }
+                    for (auto z : graph.neighborRange(p)) {
+                        if (pos[z] < pos[p]) {
+                            used[z] = false;
+                        }
+                    }
+                } else {
+                    for (auto z : graph.neighborRange(y)) {
+                        if (pos[z] < pos[y]) {
+                            used[z] = true;
+                        }
+                    }
+                    for (auto z : graph.neighborRange(p)) {
+                        if (pos[z] < pos[p] && !used[z]) {
+                            return false;
+                        }
+                    }
+                    for (auto z : graph.neighborRange(y)) {
+                        if (pos[z] < pos[y]) {
+                            used[z] = false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
 
 }  // namespace Koala
