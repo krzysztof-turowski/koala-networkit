2c31d50d92566b8c5428e00a057c34d05dee15a1
fixikmila <99081680+fixikmila@users.noreply.github.com>
2024-05-15 16:55:02 +0200

    Update benchmarkCographs.cpp

--- benchmark/benchmarkCographs.cpp
+++ benchmark/benchmarkCographs.cpp
@@ -5,12 +5,11 @@
 
 #include <iostream>
 #include <map>
+#include <string>
 
 #include "recognition/CographRecognition.hpp"
 
-int main() {
-    std::map<Koala::CorneilStewartPerlCographRecognition::State, int> classification;
-    std::string types[] = {
+std::string types[] = {
         "UNKNOWN",
         "COGRAPH",
         "CONTAINS_0_NODE",
@@ -19,8 +18,37 @@ int main() {
         "NO_ONE_PATH",
         "WRONG_PARENT",
         "WRONG_GRANDPARENT"
-    };
+};
 
+std::string default_types[] = {
+        "NOT_COGRAPH",
+        "COGRAPH"
+};
+
+std::map<std::string, int> classification;
+
+void run_algorithm(NetworKit::Graph &G, int number) {
+    if (number == 0) {
+        auto algorithm = Koala::CorneilStewartPerlCographRecognition(G);
+        algorithm.run();
+        classification[types[static_cast<int>(algorithm.getState())]]++;
+    } else if (number == 1) {
+        auto algorithm = Koala::BretscherCorneilHabibPaulCographRecognition(G);
+        algorithm.run();
+        classification[default_types[algorithm.isCograph()]]++;
+    }
+}
+
+std::map<std::string, int> ALGORITHM = {
+        { "decomposition", 0 },
+        { "lexbfs", 1 }
+};
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <algorithm>" << std::endl;
+        return 1;
+    }
     while (true) {
         std::string line;
         std::cin >> line;
@@ -28,12 +56,10 @@ int main() {
             break;
         }
         NetworKit::Graph G = Koala::G6GraphReader().readline(line);
-        auto recognize = Koala::CorneilStewartPerlCographRecognition(G);
-        recognize.run();
-        classification[recognize.getState()]++;
+        run_algorithm(G, std::stoi(argv[1]));
     }
-    for (const auto &[k, v]: classification) {
-        std::cout << types[static_cast<int>(k)] << ": " << v << std::endl;
+    for (const auto &[k, v] : classification) {
+        std::cout << k << ": " << v << std::endl;
     }
     return 0;
 }
