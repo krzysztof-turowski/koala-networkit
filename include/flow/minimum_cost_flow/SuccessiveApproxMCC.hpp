e7d8a8752e015c8616ca90954d2afd74fc98dec4
Oskar Krygier <oskar.krygier@enterit.pl>
2025-04-01 23:27:34 +0200

    Add Successive Approximation wave implementation

--- include/flow/minimum_cost_flow/SuccessiveApproxMCC.hpp
+++ include/flow/minimum_cost_flow/SuccessiveApproxMCC.hpp
@@ -2,12 +2,13 @@
 
 #include <set>
 #include <unordered_set>
+#include <list>
 
 #include <flow/MinimumCostFlow.hpp>
 
 namespace Koala {
 
-class SuccessiveApproxMCC final : public MinimumCostFlow {
+class SuccessiveApproxMCC : public MinimumCostFlow {
  public:   
     using MinimumCostFlow::MinimumCostFlow;
     SuccessiveApproxMCC::SuccessiveApproxMCC(NetworKit::Graph&, 
@@ -18,10 +19,12 @@ class SuccessiveApproxMCC final : public MinimumCostFlow {
  private:
     void runImpl();
     void initialize();
-    void push_relabel(NetworKit::node const&);
+    bool push_relabel(NetworKit::node const&);
     void push(NetworKit::node const&, NetworKit::node const&);
     void relabel(NetworKit::node const&);
     void refine();
+    void wave();
+    bool discharge(NetworKit::node const&);
 
     double cp(NetworKit::node const&, NetworKit::node const&);
     int uf(NetworKit::node const&, NetworKit::node const&);
@@ -32,6 +35,28 @@ class SuccessiveApproxMCC final : public MinimumCostFlow {
     std::unordered_set<NetworKit::node> active;
 
     double epsi{0.};
+
+    class DischargeList {
+     public:
+        virtual NetworKit::node getNext();
+        virtual void moveToStart();
+    };
+
+    class ToposortList : public DischargeList {
+        ToposortList(SuccessiveApproxMCC&);
+        
+        NetworKit::node getNext();
+        void moveToStart();
+     private:
+        SuccessiveApproxMCC &approx;
+        std::list<NetworKit::node> nodes;
+        node_map<bool> vis; 
+        void dfs(NetworKit::node);
+
+        std::list<NetworKit::node>::iterator it1;
+        std::list<NetworKit::node>::iterator it2;
+    };
+
 };
 
 } /* namespace Koala */
