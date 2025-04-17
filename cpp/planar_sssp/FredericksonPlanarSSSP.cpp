c8e9862da61bcd38d3b893683ab760f8ee777267
289Adam289 <szwajaadam@gmail.com>
2025-04-17 23:43:42 +0200

    Section 3 wip

--- cpp/planar_sssp/FredericksonPlanarSSSP.cpp
+++ cpp/planar_sssp/FredericksonPlanarSSSP.cpp
@@ -9,12 +9,75 @@
 #include <networkit/graph/DFS.hpp>
 #include <stdexcept>
 #include <cmath>
+#include <algorithm>
 
-using graphDivision_t = std::vector<std::vector<NetworKit::node>>;
+using nodeSubsets_t = std::vector<std::vector<NetworKit::node>>;
 
 namespace Koala {
 
     NetworKit::node rootNode = 0;
+    std::vector<int> isBoundry;
+    const int c = 10;
+
+    NetworKit::Graph convertToMaxDegree3(NetworKit::Graph& G) {
+        planar_embedding_t embedding = findPlanarEmbeding(G);
+        NetworKit::count result_size = G.numberOfNodes();
+
+        for (auto [i, node_embeding] : embedding) {
+            if (node_embeding.size() > 3) {
+                result_size += node_embeding.size() - 1;
+            }
+        }
+
+        if (result_size == G.numberOfNodes()) {
+            return G;
+        }
+
+        std::vector<std::unordered_map<size_t, size_t>> vertexMaps(
+            G.numberOfNodes(), std::unordered_map<size_t, size_t>());
+
+        size_t emptyVertex = G.numberOfNodes();
+        NetworKit::Graph result(result_size, true, false);
+
+        for (size_t i = 0; i < embedding.size(); ++i) {
+            if (embedding[i].size() > 3) {
+                size_t t = embedding[i][0];
+                vertexMaps[i][t] = i;
+                vertexMaps[i][emptyVertex] = 0;
+
+                result.addEdge(i, emptyVertex, 0);
+                for (int j = 1; j < embedding[i].size(); ++j) {
+                    t = embedding[i][j];
+                    vertexMaps[i][t] = emptyVertex;
+                    if (j < embedding[i].size() - 1) {
+                        result.addEdge(emptyVertex, emptyVertex + 1, 0);
+                    }
+                    emptyVertex++;
+                }
+
+                result.addEdge(emptyVertex - 1, i, 0);
+            }
+        }
+
+        for (const auto& edge : G.edgeWeightRange()) {
+            NetworKit::node u = edge.u;
+            NetworKit::node v = edge.v;
+            NetworKit::edgeweight ew = edge.weight;
+
+            size_t s = v;
+            size_t t = u;
+            if (vertexMaps[u].find(v) != vertexMaps[u].end()) {
+                t = vertexMaps[u][v];
+            }
+            if (vertexMaps[v].find(u) != vertexMaps[v].end()) {
+                s = vertexMaps[v][u];
+            }
+            result.addEdge(s, t, ew);
+        }
+
+        return result;
+    }
+
 
     struct Separator {
         std::unordered_set<NetworKit::node> A;
@@ -145,64 +208,6 @@ namespace Koala {
         throw std::runtime_error("Acording to Lipton and Tarjan Lemma 2 Should not have happened!!!");
     }
 
-    NetworKit::Graph convertToMaxDegree3(NetworKit::Graph& G) {
-        planar_embedding_t embedding = findPlanarEmbeding(G);
-        NetworKit::count result_size = G.numberOfNodes();
-
-        for (auto [i, node_embeding] : embedding) {
-            if (node_embeding.size() > 3) {
-                result_size += node_embeding.size() - 1;
-            }
-        }
-
-        if (result_size == G.numberOfNodes()) {
-            return G;
-        }
-
-        std::vector<std::unordered_map<size_t, size_t>> vertexMaps(
-            G.numberOfNodes(), std::unordered_map<size_t, size_t>());
-
-        size_t emptyVertex = G.numberOfNodes();
-        NetworKit::Graph result(result_size, true, false);
-
-        for (size_t i = 0; i < embedding.size(); ++i) {
-            if (embedding[i].size() > 3) {
-                size_t t = embedding[i][0];
-                vertexMaps[i][t] = i;
-                vertexMaps[i][emptyVertex] = 0;
-
-                result.addEdge(i, emptyVertex, 0);
-                for (int j = 1; j < embedding[i].size(); ++j) {
-                    t = embedding[i][j];
-                    vertexMaps[i][t] = emptyVertex;
-                    if (j < embedding[i].size() - 1) {
-                        result.addEdge(emptyVertex, emptyVertex + 1, 0);
-                    }
-                    emptyVertex++;
-                }
-
-                result.addEdge(emptyVertex - 1, i, 0);
-            }
-        }
-
-        for (const auto& edge : G.edgeWeightRange()) {
-            NetworKit::node u = edge.u;
-            NetworKit::node v = edge.v;
-            NetworKit::edgeweight ew = edge.weight;
-
-            size_t s = v;
-            size_t t = u;
-            if (vertexMaps[u].find(v) != vertexMaps[u].end()) {
-                t = vertexMaps[u][v];
-            }
-            if (vertexMaps[v].find(u) != vertexMaps[v].end()) {
-                s = vertexMaps[v][u];
-            }
-            result.addEdge(s, t, ew);
-        }
-
-        return result;
-    }
 
     std::vector<std::vector<NetworKit::node>> postProcesing(Separator& sep, NetworKit::Graph& G) {
         auto [A, B, C] = sep;
@@ -229,10 +234,10 @@ namespace Koala {
         }
         NetworKit::ConnectedComponents cc(copyG);
         cc.run();
-        std::vector<std::vector<NetworKit::node>> components = cc.getComponents();
+        nodeSubsets_t components = cc.getComponents();
 
         for (auto c : Cbis) {
-
+            isBoundry[c] = 1;
             for (auto x : G.neighborRange(c)) {
                 NetworKit::count componentNumber = cc.componentOfNode(x);
                 if (components[componentNumber].back() != c) {
@@ -243,19 +248,24 @@ namespace Koala {
         return components;
     }
 
-    std::vector<std::vector<NetworKit::node>> createConnectedSets(NetworKit::Graph& subGraph) {
+    nodeSubsets_t createConnectedSets(NetworKit::Graph& subGraph) {
         auto separator = findSeparator(subGraph);
 
         return postProcesing(separator, subGraph);
     }
 
-    std::vector<std::unordered_set<NetworKit::node>> makeSuitableGraphDivision(const NetworKit::Graph& Graph, int r) {
-        std::vector<std::vector<NetworKit::node>> smallSets;
-        std::vector<std::vector<NetworKit::node>> smallAndAloneSets;
-        std::vector<std::vector<NetworKit::node>> result;
+    bool canComponentBeMerged(int componentSize, int boundryNodeNum, int r, int sqr) {
+        if (componentSize < r / 2 && boundryNodeNum < c * sqr) {
+            return true;
+        }
+        return false;
+    }
 
-        std::queue<std::vector<NetworKit::node>> queue;
-        queue.push(std::vector<NetworKit::node> { Graph.nodeRange().begin(), Graph.nodeRange().end() });
+    nodeSubsets_t makeSuitableGraphDivisionFromQueue(std::queue<std::vector<NetworKit::node>> queue, NetworKit::Graph Graph, int r) {
+        int sqr = sqrt(r);
+        nodeSubsets_t smallSets;
+        nodeSubsets_t smallAndAloneSets;
+        nodeSubsets_t result;
 
         while (!queue.empty()) {
             auto nodeSet = queue.front();
@@ -265,60 +275,121 @@ namespace Koala {
             auto subGraph = NetworKit::GraphTools::subgraphFromNodes(Graph, set);
             auto sets = createConnectedSets(subGraph);
             for (auto& s : sets) {
-                if (s.size() > r) {
-                    queue.emplace(s);
+                int numOfBoundryNodes = 0;
+                for (auto node : s) {
+                    if (isBoundry[node]) {
+                        numOfBoundryNodes++;
+                    }
+                }
+                if (s.size() > r || numOfBoundryNodes > c * sqr) {
+                    queue.emplace(std::move(s));
                 }
                 else {
-                    smallSets.push_back(s);
+                    smallSets.push_back(std::move(s));
                 }
             }
         }
 
         std::vector<std::vector<NetworKit::count>> componentsPerNode(Graph.numberOfNodes());
+        std::vector<int> isSetUsed(smallSets.size(), 0);
+        std::vector<int> numOfBoundryNodes(smallSets.size(), 0);
         std::vector<int> used(Graph.numberOfNodes(), 0);
 
         for (int i = 0; i < smallSets.size(); i++) {
-            if (smallSets[i].size() > r / 2) {
-                result.push_back(smallSets[i]);
-                continue;
-            }
             for (auto& x : smallSets[i]) {
                 componentsPerNode[x].push_back(i);
             }
         }
-
-        for (auto components : componentsPerNode) {
-            assert(components.size() > 0 && components.size() < 4);
-
-            if (components.size() == 1) {
-                auto c = components[0];
-                if (used[c]) continue;
-                if (smallSets[c].size() > r / 2) {
-                    result.push_back(smallSets[c]);
+        for (int i = 0; i < componentsPerNode.size(); i++) {
+            if (componentsPerNode[i].size() > 1) {
+                for (auto c : componentsPerNode[i]) {
+                    numOfBoundryNodes[c] += 1;
                 }
-                else {
-                    smallAndAloneSets.push_back(smallSets[c]);
-                }
-            };
-
-
-            if (components.size() == 2) {
-                auto c0 = components[0];
-                auto c1 = components[1];
-
-            }
-
-            if (components.size() == 3) {
-
             }
         }
 
-        //TODO finish section 3
+        auto tryMergeComponents = [&](int c1, int c2) {
+            if (isSetUsed[c1] || isSetUsed[c2]) return;
+            if (smallSets[c1].size() > r / 2 || numOfBoundryNodes[c1] > c * sqr) return;
+            if (smallSets[c2].size() > r / 2 || numOfBoundryNodes[c2] > c * sqr) return;
+            if (smallSets[c1].size() < smallSets[c2].size()) std::swap(c1, c2);
+            isSetUsed[c2] = true;
+            int goodBoundryNodes = 0;
+            for (auto node : smallSets[c2]) {
+                if (std::find(componentsPerNode[node].begin(), componentsPerNode[node].end(), c1) == componentsPerNode[node].end()) {
+                    smallSets[c1].push_back(node);
+                    if (componentsPerNode[node].size() > 1) {
+                        goodBoundryNodes++;
+                    }
+                    std::replace(componentsPerNode[node].begin(), componentsPerNode[node].end(), c2, c1);
+                }
+                //componentsPerNode[node] can be fixed here to maybe speed up the function
+            }
+            numOfBoundryNodes[c1] += goodBoundryNodes;
+            };
+
+        //mergin components with common boundy nodes
+        for (auto& components : componentsPerNode) {
+            assert(components.size() > 0 && components.size() < 4);
+            if (components.size() == 1) continue;
+
+            if (components.size() == 2) {
+                tryMergeComponents(components[0], components[1]);
+                continue;
+            }
+            tryMergeComponents(components[0], components[2]);
+            tryMergeComponents(components[1], components[2]);
+        }
+        //filter sets that still can be matched
+
+        for (int i = 0; i < componentsPerNode.size(); i++) {
+            componentsPerNode[i].clear();
+        }
+        for (int i = 0; i < smallSets.size(); i++) {
+            if (isSetUsed[i]) continue;
+            for (auto& x : smallSets[i]) {
+                componentsPerNode[x].push_back(i);
+            }
+        }
+        for (int i = 0; i < smallSets.size(); i++) {
+            if (isSetUsed[i]) continue;
+            if (smallSets[i].size() > r / 2 || numOfBoundryNodes[i] > c * sqr) {
+                result.push_back(std::move(smallSets[i]));
+            }
+            else {
+                smallAndAloneSets.push_back(std::move(smallSets[i]));
+            }
+        }
+
+        std::vector<std::vector<int>> neighbours(smallAndAloneSets.size());
+
+        for (int i = 0; i < smallAndAloneSets.size(); i++) {
+            std::unordered_set<int> neighboursUS;
+            for (auto node : smallAndAloneSets[i]) {
+                for (auto n : componentsPerNode[node]) {
+                    neighboursUS.insert(n);
+                }
+            }
+            if (neighboursUS.size() > 2) {
+                result.push_back(std::move(smallAndAloneSets[i]));
+            }
+            neighbours[i].assign(neighboursUS.begin(), neighboursUS.end());
+        }
+
+        //TODO: merge components same set of neighbouring components
+        return result;
+    }
+
+    nodeSubsets_t makeSuitableGraphDivision(const NetworKit::Graph& Graph, int r) {
+        std::queue<std::vector<NetworKit::node>> queue;
+        queue.push(std::vector<NetworKit::node> { Graph.nodeRange().begin(), Graph.nodeRange().end() });
+
+        return makeSuitableGraphDivisionFromQueue(queue, Graph, r);
     }
 
 
     //FIND_CLUSTERS from [F1]
-    graphDivision_t findClusterResult;
+    nodeSubsets_t findClusterResult;
     std::vector<int> visited;
 
     std::vector<NetworKit::node> csearch(NetworKit::node v, int z, NetworKit::Graph& Graph) {
@@ -342,7 +413,7 @@ namespace Koala {
         }
     }
 
-    graphDivision_t findClusters(NetworKit::Graph& Graph, int z) {
+    nodeSubsets_t findClusters(NetworKit::Graph& Graph, int z) {
         findClusterResult.clear();
         visited.assign(Graph.numberOfNodes(), 0);
 
@@ -355,12 +426,18 @@ namespace Koala {
         return findClusterResult;
     }
 
-    graphDivision_t getDivisionOfFromClusters(graphDivision_t& clusters, NetworKit::Graph& Graph, int r) {
+    nodeSubsets_t getDivisionOfFromClusters(nodeSubsets_t& clusters, NetworKit::Graph& Graph, int r) {
+        std::vector<NetworKit::count> clusterNum(Graph.numberOfNodes());
+        for (int i = 0; i < clusters.size(); i++) {
+            for (auto node : clusters[i]) {
+                clusterNum[node] = i;
+            }
+        }
         //build new shrinked graph based on clusters
         NetworKit::Graph ShrinkedGraph(clusters.size() + 1);
 
         Graph.forNodes([&](auto v) {
-            for (auto& u : Graph.neighborRange(v)) {
+            for (auto u : Graph.neighborRange(v)) {
                 if (clusterNum[v] != clusterNum[u]) {
                     ShrinkedGraph.addEdge(clusterNum[v], clusterNum[u]);
                 }
@@ -380,7 +457,7 @@ namespace Koala {
             }
         }
 
-        graphDivision_t result(division.size(), std::vector<NetworKit::node> {});
+        nodeSubsets_t result(division.size(), std::vector<NetworKit::node> {});
 
         //Un Shrink the graph
         for (int i = 0; i < countRegionsOfNode.size(); i++) {
@@ -402,23 +479,17 @@ namespace Koala {
         return result;
     }
 
-    void fixGraphDivision(graphDivision_t& division, NetworKit::Graph& Graph) {
+    void fixGraphDivision(nodeSubsets_t& division, NetworKit::Graph& Graph) {
         //TODO implement infering boundy nodes
     }
 
     //create suitable r-division quickly.
-    graphDivision_t  findSuitableRDivision(NetworKit::Graph Graph, int r) {
+    nodeSubsets_t  findSuitableRDivision(NetworKit::Graph Graph, int r) {
         int rSqrt = sqrt(r);
 
         auto clusters = findClusters(Graph, rSqrt);
-        std::vector<NetworKit::count> clusterNum(Graph.numberOfNodes());
-        for (int i = 0; i < clusters.size(); i++) {
-            for (auto node : clusters[i]) {
-                clusterNum[node] = i;
-            }
-        }
 
-        graphDivision_t division = getDivisionOfFromClusters(clusters, Graph, r);
+        nodeSubsets_t division = getDivisionOfFromClusters(clusters, Graph, r);
         //infer boundry nodes - whatever that means...
         fixGraphDivision(division, Graph);
 
@@ -426,31 +497,28 @@ namespace Koala {
         //run make Suitable Graph Divison from given regions
     }
 
+    void getDistancebetweenBoundryNodes(NetworKit::Graph Graph, nodeSubsets_t division) {
+
+    }
+
+    void mainThrust(NetworKit::Graph& Graph, int s, int t) {
+
+    }
+
     void FredericksonPlanarSSSP::run() {
 
         printGraph(graph);
-
         normal_graph = convertToMaxDegree3(graph);
         printGraph(normal_graph);
+        isBoundry.assign(normal_graph.numberOfNodes(), 0);
 
+        int r = log(normal_graph.numberOfNodes());
+        auto division = findSuitableRDivision(normal_graph, r);
 
+        getDistancebetweenBoundryNodes(graph, division);
 
-        std::cout << "------------------------------------------" << std::endl;
-        auto [a, b, c] = findSeparator(normal_graph);
-
-        for (auto x : a) {
-            std::cout << x << " ";
-        }std::cout << std::endl;
-        for (auto x : b) {
-            std::cout << x << " ";
-        }std::cout << std::endl;
-        for (auto x : c) {
-            std::cout << x << " ";
-        }std::cout << std::endl;
-
-        std::vector<std::unordered_set<NetworKit::node>> Sets;
-        std::queue<std::unordered_set<NetworKit::node>> setQueue;
-
+        int s = 0, t = normal_graph.numberOfNodes();
+        mainThrust(normal_graph, s, t);
 
         hasRun = true;
         distanceToTarget = 5;
