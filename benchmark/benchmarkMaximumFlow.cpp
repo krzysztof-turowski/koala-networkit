7fca95295070d97baf25d21983f8c7b31ad10049
bartefix <bart@DESKTOP-5T2C9KL.localdomain>
2025-07-02 12:58:29 +0200

    added text

--- benchmark/benchmarkMaximumFlow.cpp
+++ benchmark/benchmarkMaximumFlow.cpp
@@ -9,26 +9,29 @@
 
 int main(int argc, char **argv) {
     if (argc == 1) {
-        std::cout << "Directory name is empty" << std::endl;
+        std::cout << "File name is empty" << std::endl;
         return 0;
     }
-    std::string directory_path(argv[1]);
-    if (!std::filesystem::exists(directory_path)) {
-        std::cout << "Directory " << directory_path << " does not exist" << std::endl;
-        return 0;
-    }
-    if (!std::filesystem::is_directory(directory_path)) {
-        std::cout << directory_path << " is not a directory" << std::endl;
+    if (argc == 2) {
+        std::cout << "Algorithm not specified" << std::endl;
         return 0;
     }
 
-    for (const auto& entry : std::filesystem::directory_iterator(directory_path)) {
-        if (entry.is_regular_file()) {
-            std::string path = entry.path().string();
-            std::cout << "\nProcessing file: " << path << std::endl;
+    std::string file_path(argv[1]);
+    int algo_num = std::stoi(argv[2]);
+    if (!std::filesystem::exists(file_path)) {
+        std::cout << "File " << file_path << " does not exist" << std::endl;
+        return 0;
+    }
+    if (std::filesystem::is_directory(file_path)) {
+        std::cout << file_path << " is a directory" << std::endl;
+        return 0;
+    }
 
+    std::cout << "\nProcessing file: " << file_path << std::endl;
+        if(algo_num == 1)
             {
-                auto [G, s, t] = Koala::DimacsGraphReader().read_all(path);
+                auto [G, s, t] = Koala::DimacsGraphReader().read_all(file_path);
                 std::cout << "Running PushRelabel..." << std::endl;
                 auto start = std::chrono::high_resolution_clock::now();
 
@@ -40,9 +43,9 @@ int main(int argc, char **argv) {
                 std::cout << "PushRelabel: Maximum flow = " << maximum_flow.getFlowSize() 
                           << ", Time taken = " << elapsed.count() << " seconds\n";
             }
-
+        if(algo_num == 2)
             {
-                auto [G, s, t] = Koala::DimacsGraphReader().read_all(path);
+                auto [G, s, t] = Koala::DimacsGraphReader().read_all(file_path);
                 std::cout << "Running BKFlow..." << std::endl;
                 auto start = std::chrono::high_resolution_clock::now();
 
@@ -54,9 +57,9 @@ int main(int argc, char **argv) {
                 std::cout << "BKFlow: Maximum flow = " << maximum_flow.getFlowSize() 
                           << ", Time taken = " << elapsed.count() << " seconds\n";
             }
-
+        if(algo_num == 3)
             {
-                auto [G, s, t] = Koala::DimacsGraphReader().read_all(path);
+                auto [G, s, t] = Koala::DimacsGraphReader().read_all(file_path);
                 std::cout << "Running MKMFlow..." << std::endl;
                 auto start = std::chrono::high_resolution_clock::now();
 
@@ -68,21 +71,19 @@ int main(int argc, char **argv) {
                 std::cout << "MKMFlow: Maximum flow = " << maximum_flow.getFlowSize() 
                           << ", Time taken = " << elapsed.count() << " seconds\n";
             }
+        if(algo_num == 4)
+            {
+                auto [G, s, t] = Koala::DimacsGraphReader().read_all(file_path);
+                std::cout << "Running KingRaoTarjanMaximumFlow..." << std::endl;
+                auto start = std::chrono::high_resolution_clock::now();
 
-            // {
-            //     auto [G, s, t] = Koala::DimacsGraphReader().read_all(path);
-            //     std::cout << "Running KingRaoTarjanMaximumFlow..." << std::endl;
-            //     auto start = std::chrono::high_resolution_clock::now();
+                auto maximum_flow = Koala::KingRaoTarjanMaximumFlow(G, s, t);
+                maximum_flow.run();
 
-            //     auto maximum_flow = Koala::KingRaoTarjanMaximumFlow(G, s, t);
-            //     maximum_flow.run();
-
-            //     auto end = std::chrono::high_resolution_clock::now();
-            //     std::chrono::duration<double> elapsed = end - start;
-            //     std::cout << "KingRaoTarjanMaximumFlow: Maximum flow = " << maximum_flow.getFlowSize() 
-            //               << ", Time taken = " << elapsed.count() << " seconds\n";
-            // }
-        }
-    }
+                auto end = std::chrono::high_resolution_clock::now();
+                std::chrono::duration<double> elapsed = end - start;
+                std::cout << "KingRaoTarjanMaximumFlow: Maximum flow = " << maximum_flow.getFlowSize() 
+                          << ", Time taken = " << elapsed.count() << " seconds\n";
+            }
     return 0;
 }
