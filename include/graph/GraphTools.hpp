65a223ddf319115d4425708420df9bc2dce847fc
Krzysztof Turowski <krzysztof.szymon.turowski@gmail.com>
2026-05-30 09:46:05 +0200

    Refactor MST benchmark

--- include/graph/GraphTools.hpp
+++ include/graph/GraphTools.hpp
@@ -18,6 +18,11 @@ NetworKit::Graph toComplement(const NetworKit::Graph&);
 NetworKit::Graph convertDirectedGraphToUndirected(const NetworKit::Graph&, bool weighted = false);
 NetworKit::Graph convertUndirectedGraphToDirected(const NetworKit::Graph&, bool weighted = false);
 
+bool isConnected(const NetworKit::Graph&);
+NetworKit::Graph makeConnected(const NetworKit::Graph&);
+bool hasDistinctIntegerWeights(const NetworKit::Graph&);
+NetworKit::Graph makeDistinctIntegerWeights(const NetworKit::Graph&, NetworKit::edgeweight);
+
 void printGraph(const NetworKit::Graph&);
 
 }  // namespace GraphTools
