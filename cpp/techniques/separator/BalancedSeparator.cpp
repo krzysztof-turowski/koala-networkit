be4f9735ed72f60349e83b50d8803ff5646bcd58
_amlq <kuba.janor@gmail.com>
2026-09-13 19:55:47 +0200

    Fix compilation issues, due to bad namespace

--- cpp/techniques/separator/BalancedSeparator.cpp
+++ cpp/techniques/separator/BalancedSeparator.cpp
@@ -1,24 +1,27 @@
 #include "techniques/separator/BalancedSeparator.hpp"
 
-BalancedSeparator::BalancedSeparator(const NetworKit::Graph& graph)
-    : graph(graph) {}
+namespace Koala {
 
-const BalancedSeparator::Partition& BalancedSeparator::getPartition() const {
+BalancedSeparator::BalancedSeparator(const NetworKit::Graph &graph) : graph(graph) {}
+
+const BalancedSeparator::Partition &BalancedSeparator::getPartition() const {
     assureFinished();
     return partition;
 }
 
-const std::vector<NetworKit::node>& BalancedSeparator::getSeparator() const {
+const std::vector<NetworKit::node> &BalancedSeparator::getSeparator() const {
     assureFinished();
     return partition.separator;
 }
 
-const std::vector<NetworKit::node>& BalancedSeparator::getPartitionA() const {
+const std::vector<NetworKit::node> &BalancedSeparator::getPartitionA() const {
     assureFinished();
     return partition.A;
 }
 
-const std::vector<NetworKit::node>& BalancedSeparator::getPartitionB() const {
+const std::vector<NetworKit::node> &BalancedSeparator::getPartitionB() const {
     assureFinished();
     return partition.B;
 }
+
+} // namespace Koala
